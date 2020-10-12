4e9e495e4b75db3a69cfa5e541cc0e46000d8f76
Alina Gendel <alina.gendel@sagebase.org>
2020-10-12 13:14:21 -0700

    proper async hook

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -1,14 +1,13 @@
-//@ts-nocheck
-
 import {
   Response,
- // SignInData,
+  // SignInData,
   LoggedInUserData,
   //SignInDataEmail,
 
- // LoginType,
-  StringDictionary,RequestStatus, Study
-
+  // LoginType,
+  StringDictionary,
+  RequestStatus,
+  Study,
 } from '../types/types'
 
 import CONSTANTS from '../types/constants'
@@ -89,7 +88,7 @@ export const callEndpoint = async <T>(
 ): Promise<Response<T>> => {
   const ls = window.localStorage
   const isE2E = ls.getItem('crc_e2e')
-  let url =  `${CONSTANTS.constants.ENDPOINT}${endpoint}`
+  let url = `${CONSTANTS.constants.ENDPOINT}${endpoint}`
   if (isE2E) {
     return callEndpointXHR(url, method, data, token)
   }
@@ -143,7 +142,6 @@ export const setSession = (data: SessionData) => {
   sessionStorage.setItem(CONSTANTS.constants.SESSION_NAME, JSON.stringify(data))
 }
 
-
 export const getSearchParams = (search: string): { [key: string]: string } => {
   const searchParamsProps: any = {}
   // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams -- needs polyfill for ie11
@@ -191,33 +189,33 @@ export const useSessionStorage = (
   return [storedValue, setValue]
 }
 
-
-export const getRandomId=(): string => {
-  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
-  console.log('newId', uint32.toString(16) )
-  return uint32.toString(16);
+export const getRandomId = (): string => {
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0]
+  console.log('newId', uint32.toString(16))
+  return uint32.toString(16)
 }
-
-
 
 //async hooks
 
 interface AsyncAction<T> {
   type: RequestStatus
-  data: T| null
+  data: T | null
   error?: Error | null
 }
 
 interface AsyncReturnType<T> {
+  setData: Function
+  setError: Function
   error?: Error | null | undefined
   status: RequestStatus
   data: T | null
   run: Function
-
 }
 
-type HookState<T> ={
-  status: RequestStatus, data: T| null, error?: Error| null| undefined
+type HookState<T> = {
+  status: RequestStatus
+  data: T | null
+  error?: Error | null | undefined
 }
 
 function useSafeDispatch<T>(dispatch: Function): Function {
@@ -230,7 +228,9 @@ function useSafeDispatch<T>(dispatch: Function): Function {
   // interact with the dom may depend on the value being set
   React.useEffect(() => {
     mountedRef.current = true
-    return () => {mountedRef.current = false}
+    return () => {
+      mountedRef.current = false
+    }
   }, [])
 
   return React.useCallback(
@@ -239,18 +239,16 @@ function useSafeDispatch<T>(dispatch: Function): Function {
   )
 }
 
-
-
 function asyncReducer<T>(_state: any, action: AsyncAction<T>): HookState<T> {
   switch (action.type) {
     case 'PENDING': {
-      return {status: 'PENDING', data: null, error: null}
+      return { status: 'PENDING', data: null, error: null }
     }
     case 'RESOLVED': {
-      return {status: 'RESOLVED', data: action.data, error: null}
+      return { status: 'RESOLVED', data: action.data, error: null }
     }
     case 'REJECTED': {
-      return {status: 'REJECTED', data: null, error: action.error}
+      return { status: 'REJECTED', data: null, error: action.error }
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -258,76 +256,51 @@ function asyncReducer<T>(_state: any, action: AsyncAction<T>): HookState<T> {
   }
 }
 
-export function useAsync<T>(initialState?: HookState<T>): AsyncReturnType<T>{
+export function useAsync<T>(initialState?: HookState<T>): AsyncReturnType<T> {
   const initState: HookState<T> = {
     status: 'IDLE',
-    data: null ,
+    data: null,
     error: null,
     ...initialState,
   }
-  const [state, unsafeDispatch] = React.useReducer<React.Reducer<HookState<T>, AsyncAction<T>>>(asyncReducer, initState)
+  const [state, unsafeDispatch] = React.useReducer<
+    React.Reducer<HookState<T>, AsyncAction<T>>
+  >(asyncReducer, initState)
 
   const dispatch = useSafeDispatch<AsyncAction<T>>(unsafeDispatch)
 
-  const {data, error, status} = state
+  const { data, error, status } = state
 
   const run = React.useCallback(
-    (promise) => {
-      dispatch({type: 'PENDING'})
+    promise => {
+      dispatch({ type: 'PENDING' })
       promise.then(
-       (data: Study) => {
-          dispatch({type: 'RESOLVED', data})
+        (data: T) => {
+          dispatch({ type: 'RESOLVED', data })
         },
         (error: Error) => {
-          dispatch({type: 'REJECTED', error})
+          dispatch({ type: 'REJECTED', error })
         },
       )
     },
     [dispatch],
   )
 
+  const setData = React.useCallback(
+    data => dispatch({ type: 'RESOLVED', data }),
+    [dispatch],
+  )
+  const setError = React.useCallback(
+    error => dispatch({ type: 'REJECTED', error }),
+    [dispatch],
+  )
+
   return {
+    setData,
+    setError,
     error,
     status,
     data,
     run,
   }
 }
-
-function useWhyDidYouUpdate(name: any, props:any) {
-  // Get a mutable ref object where we can store props ...
-  // ... for comparison next time this hook runs.
-  const previousProps = React.useRef();
-
-  React.useEffect(() => {
-    if (previousProps.current) {
-      // Get all keys from previous and current props
-
-      //@ts-ignore
-      const allKeys = Object.keys({ ...previousProps.current, ...props });
-      // Use this object to keep track of changed props
-      const changesObj = {};
-      // Iterate through keys
-      allKeys.forEach(key => {
-        // If previous is different from current
-         //@ts-ignore
-        if (previousProps.current[key] !== props[key]) {
-          // Add to changesObj
-          changesObj[key] = {
-            from: previousProps.current[key],
-            to: props[key]
-          };
-        }
-      });
-
-      // If changesObj not empty then output to console
-      if (Object.keys(changesObj).length) {
-        console.log('[why-did-you-update]', name, changesObj);
-      }
-    }
-
-    // Finally update previousProps with current props for next hook call
-    previousProps.current = props;
-  });
-}
-
