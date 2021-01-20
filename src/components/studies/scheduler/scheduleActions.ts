c7236c45d1af149502a7f57d20ee6000a82564dd
Alina Gendel <alina.gendel@sagebase.org>
2021-01-20 15:59:55 -0800

    update schedule actions

--- src/components/studies/scheduler/scheduleActions.ts
+++ src/components/studies/scheduler/scheduleActions.ts
@@ -1,6 +1,4 @@
-import { getRandomId } from '../../../helpers/utility'
 import { SessionSchedule, StudySession } from '../../../types/scheduling'
-import { Assessment } from '../../../types/types'
 
 type ActionMap<M extends { [index: string]: any }> = {
   [Key in keyof M]: M[Key] extends undefined
@@ -14,22 +12,9 @@ type ActionMap<M extends { [index: string]: any }> = {
 }
 
 export enum ActionTypes {
-  /*SetSessions = 'SET_SESSIONS',
-
-  AddSession = 'ADD_SESSION',
-  RemoveSession = 'REMOVE_SESSION',
-  SetActiveSession = 'SEAT_ACTIVE_SESSION',
-  UpdateSessionName = 'UPDATE_SESSION_NAME',
-  UpdateAssessments = 'UPDATE_ASSESSMENTS',*/
   UpdateSessionSchedule = 'UPDATE_SCHEDULE',
 }
-/*
-export type Schedule = {
-  name: string
-  startEventId: string
-  sessions: StudySession[]
-}
-*/
+
 export type ActionPayload = {
   [ActionTypes.UpdateSessionSchedule]: {
     sessionId: string
@@ -37,70 +22,7 @@ export type ActionPayload = {
   }
 }
 
-export type SessionScheduleAction = ActionMap<ActionPayload>[keyof ActionMap<
-  ActionPayload
->]
-
-
-function addSession(
-  sessions: StudySession[],
-  name: string,
-  assessments: Assessment[],
-  studyId: string,
-  isActive: boolean = false,
-): StudySession[] {
-  const session: StudySession = {
-    id: getRandomId(),
-    assessments,
-    active: isActive,
-    studyId,
-    order: sessions.length,
-    //duration: 0,
-    name,
-  }
-
-  const result = [
-    ...sessions.map((session, index) => ({
-      ...session,
-      active: false,
-      order: index,
-    })),
-    {
-      ...session,
-    },
-  ]
-
-  console.log('new sessions', result)
-  return result
-}
-
-function setActiveSession(
-  sessions: StudySession[],
-  sessionId: string,
-): StudySession[] {
-  const result = sessions.map(session => ({
-    ...session,
-    active: session.id === sessionId,
-  }))
-
-  return result
-}
-
-function updateSessionName(
-  sessions: StudySession[],
-  sessionId: string,
-  sessionName: string,
-): StudySession[] {
-  const result = sessions.map(session => {
-    if (session.id !== sessionId) {
-      return session
-    } else {
-      return { ...session, name: sessionName }
-    }
-  })
-
-  return result
-}
+export type SessionScheduleAction = ActionMap<ActionPayload>[keyof ActionMap<ActionPayload>]
 
 function updateSessionSchedule(
   sessions: StudySession[],
@@ -117,63 +39,11 @@ function updateSessionSchedule(
   return result
 }
 
-/*
-function removeSession(
-  sessions: StudySession[],
-  sessionId: string,
-): StudySession[] {
-  return sessions.filter(session => session.id !== sessionId).map((s, index) =>({ ...s, order: index }))
-}
-
-function updateAssessments(
-  sessions: StudySession[],
-  sessionId: string,
-  assessments: Assessment[],
-): StudySession[] {
-  const result = sessions.map(session => {
-    if (session.id !== sessionId) {
-      return session
-    } else {
-      return { ...session, assessments: assessments }
-    }
-  })
-  return result
-}
-*/
 function actionsReducer(
   sessions: StudySession[],
   action: SessionScheduleAction,
 ): StudySession[] {
   switch (action.type) {
-    /*case Types.UpdateSessionSchedule: {
-      return action.payload.sessions
-    }
-
-   case Types.AddSession: {
-      return addSession(
-        sessions,
-
-        action.payload.name,
-        action.payload.assessments,
-        action.payload.studyId,
-        action.payload.active,
-      )
-    }
-    case Types.SetActiveSession: {
-      return setActiveSession(sessions, action.payload.sessionId)
-    }
-
-    case Types.UpdateSessionName: {
-      return updateSessionName(
-        sessions,
-        action.payload.sessionId,
-        action.payload.sessionName,
-      )
-    }
-
-    case Types.RemoveSession: {
-      return removeSession(sessions, action.payload.sessionId)
-    }*/
     case ActionTypes.UpdateSessionSchedule: {
       return updateSessionSchedule(
         sessions,
