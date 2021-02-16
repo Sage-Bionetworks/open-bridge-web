c35406964cee7cb7b2004ab15d83a5b546f44d9c
Alina Gendel <alina.gendel@sagebase.org>
2021-02-16 09:40:31 -0800

    clean up participant manager

--- src/components/studies/participants/ParticipantManager.tsx
+++ src/components/studies/participants/ParticipantManager.tsx
@@ -3,15 +3,20 @@ import { makeStyles } from '@material-ui/core/styles'
 import DeleteIcon from '@material-ui/icons/Delete'
 import React, { FunctionComponent } from 'react'
 import { useErrorHandler } from 'react-error-boundary'
-import { RouteComponentProps, useParams } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router-dom'
 import { useAsync } from '../../../helpers/AsyncHook'
 import { useUserSessionDataState } from '../../../helpers/AuthContext'
-import { useStudyBuilderInfo } from '../../../helpers/hooks'
+import {
+  StudyInfoData,
+  useStudyInfoDataDispatch,
+  useStudyInfoDataState
+} from '../../../helpers/StudyInfoContext'
 import ParticipantService from '../../../services/participants.service'
 import StudyService from '../../../services/study.service'
 import { EnrollmentType, ParticipantAccountSummary } from '../../../types/types'
 import CollapsibleLayout from '../../widgets/CollapsibleLayout'
 import HideWhen from '../../widgets/HideWhen'
+import AddByIdDialog from './AddByIdDialog'
 import AddParticipants from './AddParticipants'
 import EnrollmentSelector from './EnrollmentSelector'
 import ParticipantTableGrid from './ParticipantTableGrid'
@@ -39,26 +44,36 @@ const participantRecordTemplate: ParticipantAccountSummary = {
   id: '',
   externalIds: {},
 }
-type keys = keyof ParticipantAccountSummary
+
 
 type ParticipantManagerProps = ParticipantManagerOwnProps & RouteComponentProps
 
 const ParticipantManager: FunctionComponent<ParticipantManagerProps> = () => {
-  let { id } = useParams<{ id: string }>()
-  const [isEdit, setIsEdit] = React.useState(false)
-
   const handleError = useErrorHandler()
   const classes = useStyles()
+
+  const [isEdit, setIsEdit] = React.useState(true)
+  const [exportData, setExportData] = React.useState<any[] | null>(null)
+  const [
+    refreshParticipantsToggle,
+    setRefreshParticipantsToggle,
+  ] = React.useState(false)
+  //used with generate id enrollbyId
+  const [isGenerateIds, setIsGenerateIds] = React.useState(false)
+
+  const { study }: StudyInfoData = useStudyInfoDataState()
+  const studyDataUpdateFn = useStudyInfoDataDispatch()
+
   //if you need search params use the following
   //const { param } = useParams<{ param: string}>()
   //<T> is the type of data you are retrieving
   const { token } = useUserSessionDataState()
-  const {
+  /*const {
     data: studyData,
     status: studyStatus,
     error: studyError,
     setData: setStudyData,
-  } = useStudyBuilderInfo(id)
+  } = useStudyBuilderInfo(id)*/
   const {
     data: participantData,
     status,
@@ -69,14 +84,13 @@ const ParticipantManager: FunctionComponent<ParticipantManagerProps> = () => {
     status: 'PENDING',
     data: null,
   })
-  const [exportData, setExportData] = React.useState<any[] | null>(null)
 
-  const updateEnrollment = async(type: EnrollmentType) => {
-    let study = studyData!.study
-    study.options = {...study.options, enrollmentType: type}
+  const updateEnrollment = async (type: EnrollmentType) => {
+    study.options = { ...study.options, enrollmentType: type }
 
     const updatedStudy = await StudyService.updateStudy(study, token!)
-    setStudyData({...studyData, study})
+    studyDataUpdateFn({ type: 'SET_STUDY', payload: { study: study } })
+    // setStudyData({...studyData, study})
   }
 
   React.useEffect(() => {
@@ -96,74 +110,98 @@ const ParticipantManager: FunctionComponent<ParticipantManagerProps> = () => {
   }, [participantData])
 
   React.useEffect(() => {
-    if (!id) {
+    if (!study?.identifier) {
       return
     }
     ///your async call
-    return run(ParticipantService.getParticipants('mtb-user-testing', token!))
-  }, [id, run])
+    console.log('refreshing participant grid')
+    return run(ParticipantService.getParticipants(study.identifier, token!))
+  }, [study?.identifier, run, refreshParticipantsToggle])
 
-  if (status === 'PENDING') {
+  if (status === 'PENDING' || !study) {
     return <>loading component here</>
   } else if (status === 'REJECTED') {
     handleError(error!)
   } else if (status === 'RESOLVED') {
     return (
       <>
-
         <Box px={3} py={2}>
-          Study ID: {id}
+          Study ID: {study.identifier}
         </Box>
-        {!studyData?.study.options?.enrollmentType && <EnrollmentSelector callbackFn={(type: EnrollmentType)=> updateEnrollment(type)}></EnrollmentSelector>}
-        {studyData?.study.options?.enrollmentType &&(<>
-        {studyData.study.options.enrollmentType}
-        <Box px={3} py={2}>
-          <Grid component="label" container alignItems="center" spacing={0}>
-            <Grid item>View</Grid>
-            <Grid item>
-              <Switch
-                checked={isEdit}
-                classes={{ root: classes.switchRoot }}
-                onChange={e => setIsEdit(e.target.checked)}
-                name="viewEdit"
-              />
-            </Grid>
-            <Grid item>Edit</Grid>
-          </Grid>
-          <Box>
-            <HideWhen hideWhen={!isEdit}>
-              <div style={{ display: 'inline' }}>
-                <Button>Move to</Button>
+        {!study.options?.enrollmentType && (
+          <EnrollmentSelector
+            callbackFn={(type: EnrollmentType) => updateEnrollment(type)}
+          ></EnrollmentSelector>
+        )}
+        {study.options?.enrollmentType && (
+          <>
+            {study.options.enrollmentType}
+            <Box px={3} py={2}>
+              <Grid component="label" container alignItems="center" spacing={0}>
+                <Grid item>View</Grid>
+                <Grid item>
+                  <Switch
+                    checked={isEdit}
+                    classes={{ root: classes.switchRoot }}
+                    onChange={e => setIsEdit(e.target.checked)}
+                    name="viewEdit"
+                  />
+                </Grid>
+                <Grid item>Edit</Grid>
+              </Grid>
+              <Box>
+                <HideWhen hideWhen={!isEdit}>
+                  <div style={{ display: 'inline' }}>
+                    <Button>Move to</Button>
 
-                <Button>Unlink phone number</Button>
-                <Button>
-                  <DeleteIcon />
-                </Button>
-              </div>
-            </HideWhen>
-            <Button>Download</Button>
-          </Box>
-        </Box>
-        <CollapsibleLayout
-          expandedWidth={300}
-          isFullWidth={true}
-          isHideContentOnClose={true}
-          isDrawerHidden= {!isEdit}
-        >
-          <AddParticipants
-            studyId={id!}
-            token={token!}
-            enrollmentType={'ID'}
-          ></AddParticipants>
-          <Box py={0} pr={3} pl={2}>
-            <ParticipantTableGrid
-              rows={participantData || []}
-              studyId={'mtb-user-testing'}
-            ></ParticipantTableGrid>
-          </Box>
+                    <Button>Unlink phone number</Button>
+                    <Button>
+                      <DeleteIcon />
+                    </Button>
+                  </div>
+                </HideWhen>
+                <Button>Download</Button>
+              </Box>
+            </Box>
+            <CollapsibleLayout
+              expandedWidth={300}
+              isFullWidth={true}
+              isHideContentOnClose={true}
+              isDrawerHidden={!isEdit}
+            ><>
+              {!isGenerateIds && (
+                <AddParticipants
+                  study={study}
+                  token={token!}
+                  enrollmentType={'PHONE'/*study.options.enrollmentType*/}
+                  onAdded={(isHideAdd: boolean) => {
+                    setRefreshParticipantsToggle(prev => !prev)
+                  }}
+                ></AddParticipants>
+              )}
+              {study.options.enrollmentType === 'ID' && (
+                <AddByIdDialog
+                  study={study}
+                  token={token!}
+                  onAdded={(isHideAdd: boolean) => {
+                    setRefreshParticipantsToggle(prev => !prev)
+                    setIsGenerateIds(true)
+                  }}
+                ></AddByIdDialog>
+              )}</>
+              <Box py={0} pr={3} pl={2}>
+                <ParticipantTableGrid
+                  rows={participantData || []}
+                  studyId={study.identifier}
+                ></ParticipantTableGrid>
+              </Box>
 
-          <Box textAlign="center" pl={2}>ADD A PARTICIPANT</Box>
-        </CollapsibleLayout></>)}
+              <Box textAlign="center" pl={2}>
+                ADD A PARTICIPANT
+              </Box>
+            </CollapsibleLayout>
+          </>
+        )}
       </>
     )
   }
