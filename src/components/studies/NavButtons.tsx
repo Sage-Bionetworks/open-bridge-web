cc27104e5b1889ee2c14ce94bcb8600349f72020
Alina Gendel <alina.gendel@sagebase.org>
2021-03-23 12:48:30 -0700

    update the nav buttons

--- src/components/studies/NavButtons.tsx
+++ src/components/studies/NavButtons.tsx
@@ -1,6 +1,27 @@
-import { Box, Button } from '@material-ui/core'
+import { Box, Button, makeStyles } from '@material-ui/core'
 import React from 'react'
+import { ReactComponent as ArrowIcon } from '../../assets/arrow_long.svg'
 import { SECTIONS as sectionLinks, StudySection } from './sections'
+
+const useStyles = makeStyles(theme => ({
+  prevButton: {
+    '& svg': {
+      marginRight: theme.spacing(2),
+      '& path': {
+        fill: '#2a2a2a',
+      },
+    },
+  },
+  nextButton: {
+    '& svg': {
+      marginLeft: theme.spacing(2),
+      transform: 'scaleX(-1)',
+      '& path': {
+        fill: '#fcfcfc',
+      },
+    },
+  },
+}))
 
 export interface NavButtonsProps {
   currentSection: StudySection
@@ -13,6 +34,7 @@ const NavButtons: React.FunctionComponent<NavButtonsProps> = ({
   id,
   onNavigate,
 }: NavButtonsProps) => {
+  const classes = useStyles()
   const currentIndex = sectionLinks.findIndex(i => i.path === currentSection)
   const prev = currentIndex > 0 ? sectionLinks[currentIndex - 1] : undefined
   const next =
@@ -20,27 +42,34 @@ const NavButtons: React.FunctionComponent<NavButtonsProps> = ({
       ? sectionLinks[currentIndex + 1]
       : undefined
 
-  const NavLink = (props: any) => {
-    const {section } = props
-
-
-    if (!section) {
-      return <></>
-    }
-    return (
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => onNavigate(section.path)}
-      >
-        {section.name}
-      </Button>
-    )
-  }
   const result = (
-    <Box paddingTop={2} textAlign="right">
-      <NavLink id={id} section={prev}></NavLink>&nbsp;&nbsp;
-      <NavLink id={id} section={next}></NavLink>
+    <Box py={2} textAlign="right">
+      {prev && (
+        <>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.prevButton}
+            onClick={() => onNavigate(prev.path)}
+          >
+            <ArrowIcon /> {prev.name}
+          </Button>{' '}
+          &nbsp;&nbsp;
+        </>
+      )}
+
+      {next && (
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.nextButton}
+            onClick={() => onNavigate(next.path)}
+          >
+            {next.name} <ArrowIcon />
+          </Button>
+        </>
+      )}
     </Box>
   )
   return result
