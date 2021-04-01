54be5c753665c99e84e1486808b29549cb192445
Alina Gendel <alina.gendel@sagebase.org>
2021-03-31 19:10:35 -0700

    helpBox
    (cherry picked from commit ed0b777c20d538501287ed2efe94128680aa5777)

---
+++ src/components/widgets/HelpBox.tsx
@@ -0,0 +1,112 @@
+import { Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+import { latoFont, ThemeType } from '../../style/theme'
+
+interface StyleProps {
+  helpTextWidth: number
+  helpTextLeftOffset: number
+  helpTextTopOffset: number
+  arrowTailLength: number
+  arrowRotate: number
+}
+
+const useStyles = makeStyles<ThemeType, StyleProps>(theme => ({
+  root: {
+    background: theme.palette.error.light,
+    color: 'black',
+    borderRadius: '0',
+    fontFamily: latoFont,
+    fontSize: '14px',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    padding: theme.spacing(0.625, 2),
+
+    '&:hover': {
+      fontWeight: 'bolder',
+      background: theme.palette.error.light,
+      boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)',
+    },
+  },
+  helpArrow: {
+    transform: 'rotate(-10deg)',
+  },
+  helpText: props => ({
+    backgroundColor: '#FFE500',
+    padding: theme.spacing(2),
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)',
+    position: 'absolute',
+    top: props.helpTextTopOffset,
+    left: props.helpTextLeftOffset,
+    width: props.helpTextWidth,
+  }),
+  arrow: props => ({
+    display: 'inline-block',
+    float: 'left',
+    cursor: 'pointer',
+
+    '&:after': {
+      top: '2px',
+      left: 0,
+      width: `${props.arrowTailLength}px`,
+      height: '1px',
+      content: '""',
+      display: 'block',
+      position: 'absolute',
+      backgroundColor: 'black',
+    },
+    '&:before': {
+      border: 'solid black',
+      content: '""',
+      display: 'inline-block',
+      padding: '2px',
+      position: 'absolute',
+      transform: 'rotate(130deg)',
+      borderWidth: '0 1px 1px 0',
+    },
+  }),
+}))
+
+export interface ButtonStyleProps {
+  helpTextWidth?: number
+  helpTextLeftOffset?: number
+  helpTextTopOffset?: number
+  arrowRotate?: number
+  leftOffset?: number
+  topOffset?: number
+  arrowTailLength?: number
+  //children: ReactNode
+}
+
+const HelpBox: React.FunctionComponent<ButtonStyleProps> = ({
+  helpTextWidth = 200,
+  helpTextLeftOffset = 60,
+  arrowRotate = 20,
+  arrowTailLength = 280,
+  leftOffset = 100,
+  topOffset = 20,
+  helpTextTopOffset = 0,
+  children,
+}) => {
+  const classes = useStyles({
+    helpTextWidth,
+    helpTextLeftOffset,
+    arrowRotate,
+    arrowTailLength,
+    helpTextTopOffset,
+  })
+
+
+  return (
+    <Box zIndex="100" position="absolute" left={leftOffset} top={topOffset}>
+      <Box
+        style={{ transform: `rotate(${arrowRotate}deg)` }}
+        position="absolute"
+      >
+        <div className={classes.arrow}></div>
+      </Box>
+      <Box className={classes.helpText}>{children}</Box>
+    </Box>
+  )
+}
+
+export default HelpBox
