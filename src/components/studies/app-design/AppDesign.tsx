afd2388aeb60a63c3edb0d1f1fde4448b6707138
Leon Li <leonli3three@gmail.com>
2021-04-27 20:11:53 -0700

    Fixed error with contact lead info phone number always showing error.

--- src/components/studies/app-design/AppDesign.tsx
+++ src/components/studies/app-design/AppDesign.tsx
@@ -1183,7 +1183,7 @@ const AppDesign: React.FunctionComponent<
                         isInvalidPhone(
                           appDesignProperties.contactLeadInfo?.phone?.number ||
                             '',
-                        ) ||
+                        ) &&
                         appDesignProperties.contactLeadInfo?.phone?.number !==
                           ''
                       setPhoneNumberErrorState(prevState => {
