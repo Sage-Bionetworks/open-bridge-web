228b1ebda08e59169fa34fb445077d2b4365819e
Alina Gendel <alina.gendel@sagebase.org>
2021-05-24 15:19:33 -0700

    changeAssessmentOrder control

--- src/components/studies/scheduler/AssessmentList.tsx
+++ src/components/studies/scheduler/AssessmentList.tsx
@@ -1,20 +1,12 @@
-import {
-  Box,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  makeStyles,
-  Switch,
-  Typography
-} from '@material-ui/core'
+import { Box, FormControlLabel, FormGroup, makeStyles } from '@material-ui/core'
 import ClockIcon from '@material-ui/icons/AccessTime'
 import clsx from 'clsx'
 import React from 'react'
-import { ThemeType } from '../../../style/theme'
+import { latoFont, ThemeType } from '../../../style/theme'
 import { PerformanceOrder, StudySession } from '../../../types/scheduling'
 import { Assessment } from '../../../types/types'
 import AssessmentSmall from '../../assessments/AssessmentSmall'
+import SaveBlackBorderDropdown from '../../widgets/BlackBorderDropdown'
 import SessionIcon from '../../widgets/SessionIcon'
 
 const useStyles = makeStyles((theme: ThemeType) => ({
@@ -29,9 +21,15 @@ const useStyles = makeStyles((theme: ThemeType) => ({
   inner: {
     borderTop: '1px solid #000',
     borderBottom: '1px solid #000',
-    paddingTop: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
+    padding: theme.spacing(2),
+  },
+  order: {
+    marginRight: '16px',
+    marginTop: '16px',
+    '& span': {
+      fontSize: '11px',
+      fontFamily: latoFont,
+    },
   },
 }))
 
@@ -84,16 +82,31 @@ const AssessmentList: React.FunctionComponent<AssessmentListProps> = ({
     performanceOrder !== 'participant_choice',
   )
 
-  const getMargins = (
+  const performanceOrderList = [
+    { value: 'participant_choice', label: 'Participant Choice' },
+    { value: 'sequential', label: 'Fixed Order' },
+    { value: 'randomized', label: 'Random Order' },
+  ]
+
+  const getCardStyle = (
     index: number,
     assessmentsNumber: number,
   ): React.CSSProperties => {
     if (assessmentsNumber === 1) {
       return {}
     }
-    const mTop = `${index === 0 ? 0 : -65 * 1}px`
-    const mLeft = `${-10 * index + 50}px`
-    return { marginLeft: mLeft, marginTop: mTop }
+    const offsetUnit = -16 / assessmentsNumber
+    const mTop = `${index === 0 ? 0 : -75 * 1}px`
+
+    const mLeft = `${offsetUnit * index}px`
+    const mRight = `${offsetUnit * (assessmentsNumber - index)}px`
+    return {
+      marginLeft: mLeft,
+      marginRight: mRight,
+      marginTop: mTop,
+      boxShadow: '5px 5px 10px 0 rgb(0 0 0 / 10%)',
+      width: '248px',
+    }
   }
   return (
     <>
@@ -111,15 +124,15 @@ const AssessmentList: React.FunctionComponent<AssessmentListProps> = ({
         {studySession.assessments &&
           studySession.assessments.map((assessment, index) => (
             <Box
-              key={studySession.guid!+assessment.guid+index}
+              key={studySession.guid! + assessment.guid + index}
               style={
-                performanceOrder === 'randomized'
-                  ? getMargins(index, studySession.assessments?.length || 0)
+                performanceOrder !== 'participant_choice'
+                  ? getCardStyle(index, studySession.assessments?.length || 0)
                   : {}
               }
             >
               <AssessmentSmall
-                isHideDuration={performanceOrder === 'randomized'}
+                isHideDuration={false /*performanceOrder === 'randomized'*/}
                 assessment={assessment}
                 hasHover={false}
                 isDragging={false}
@@ -128,48 +141,23 @@ const AssessmentList: React.FunctionComponent<AssessmentListProps> = ({
           ))}
       </div>
       {studySession.assessments && studySession.assessments.length > 1 && (
-        <FormGroup aria-label="assessments" row style={{ marginLeft: '16px' }}>
+        <FormGroup aria-label="assessments" row>
           <FormControlLabel
+            labelPlacement="start"
+            className={classes.order}
             control={
-              <Checkbox
-                checked={isGroupAssessments}
-                onChange={e => {
-                  if (!e.target.checked) {
-                    onChangePerformanceOrder('participant_choice')
-                  } else {
-                    setIsGroupAssessments(true)
-                  }
-                }}
+              <SaveBlackBorderDropdown
+                width="150px"
+                value={performanceOrder}
+                onChange={e =>
+                  onChangePerformanceOrder(e.target.value as PerformanceOrder)
+                }
+                emptyValueLabel="select"
+                dropdown={performanceOrderList}
               />
             }
-            label="Bundle assessments"
+            label="Assessment Order:"
           />
-
-          <Typography component="div" style={{ marginLeft: '30px' }}>
-            <Grid
-              component="label"
-              container
-              alignItems="center"
-              spacing={1}
-              wrap="nowrap"
-            >
-              <Grid item>Sequential</Grid>
-              <Grid item>
-                <Switch
-                  color="primary"
-                  disabled={!isGroupAssessments}
-                  checked={performanceOrder === 'randomized'}
-                  onChange={e => {
-                    e.target.checked
-                      ? onChangePerformanceOrder('randomized')
-                      : onChangePerformanceOrder('sequential')
-                  }}
-                  name="checkedC"
-                />
-              </Grid>
-              <Grid item>Randomized</Grid>
-            </Grid>
-          </Typography>
         </FormGroup>
       )}
     </>
