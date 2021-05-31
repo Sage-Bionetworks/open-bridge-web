50a6440dbf6337fba3a4647693a474e7e70d4020
Leon Li <leonli3three@gmail.com>
2021-05-31 09:20:40 -0700

    Did some basic restyling to the format of the text boxes.

--- src/components/studies/launch/IrbDetails.tsx
+++ src/components/studies/launch/IrbDetails.tsx
@@ -6,7 +6,7 @@ import {
   FormControlLabel,
   Grid,
   Radio,
-  RadioGroup
+  RadioGroup,
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
@@ -20,9 +20,11 @@ import DatePicker from '../../widgets/DatePicker'
 import { MTBHeadingH1, MTBHeadingH2 } from '../../widgets/Headings'
 import {
   SimpleTextInput,
-  SimpleTextLabel
+  SimpleTextLabel,
+  AlertWithText,
 } from '../../widgets/StyledComponents'
 import LeadInvestigatorDropdown from '../app-design/LeadInvestigatorDropdown'
+import Alert_Icon from '../../../assets/alert_icon.svg'
 
 const useStyles = makeStyles((theme: ThemeType) => ({
   root: {
@@ -38,6 +40,9 @@ const useStyles = makeStyles((theme: ThemeType) => ({
         fill: '#fcfcfc',
       },
     },
+  },
+  input: {
+    height: '44px',
   },
 }))
 
@@ -91,46 +96,47 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
       {!isFinished && (
         <Box textAlign="left">
           <MTBHeadingH2>IRB Details &amp; Certification</MTBHeadingH2>
-          <div>I certify that</div>
-
-          <FormControlLabel
-            key="confirmReviewed"
-            control={
-              <Checkbox
-                checked={true}
-                onChange={event => {
-                  if (event.target.checked) {
-                  }
-                }}
-                name="confirmReviewed"
-                color="primary"
-              />
-            }
-            label="The study protocol was reviewed by the IRB and /or equivalent body listed below."
-          />
-
-          <FormControlLabel
-            key="confirmConsistent"
-            control={
-              <Checkbox
-                checked={true}
-                onChange={event => {
-                  if (event.target.checked) {
-                  }
-                }}
-                name="confirmReviewed"
-                color="primary"
-              />
-            }
-            label="The Principal Investigator has assured that this study is consistent with applicable laws and regulations as well as relevant institutional policies.
-        "
-          />
+          <Box mt={2} mb={2} fontSize="16px" fontFamily="Lato">
+            I certify that
+          </Box>
+          <Box pl={3} mb={4}>
+            <FormControlLabel
+              key="confirmReviewed"
+              control={
+                <Checkbox
+                  checked={true}
+                  onChange={event => {
+                    if (event.target.checked) {
+                    }
+                  }}
+                  name="confirmReviewed"
+                  color="primary"
+                />
+              }
+              label="The study protocol was reviewed by the IRB and /or equivalent body listed below."
+            />
+            <FormControlLabel
+              key="confirmConsistent"
+              control={
+                <Checkbox
+                  checked={true}
+                  onChange={event => {
+                    if (event.target.checked) {
+                    }
+                  }}
+                  name="confirmReviewed"
+                  color="primary"
+                />
+              }
+              label="The Principal Investigator has assured that this study is consistent with applicable laws and regulations as well as relevant institutional policies."
+            />
+          </Box>
 
           <Grid container spacing={4}>
             <Grid item xs={6}>
               <FormControl fullWidth>
                 <SimpleTextLabel htmlFor="protocolTitle">
-                  IRB Protocol Title
+                  IRB Protocol Title*
                 </SimpleTextLabel>
                 <SimpleTextInput
                   value=""
@@ -138,13 +144,12 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
                   onChange={e => {}}
                   id="protocolTitle"
                   rows={5}
+                  className={classes.input}
                 />
               </FormControl>
             </Grid>
             <Grid item xs={6}></Grid>
             <Grid item xs={6}>
-              {' '}
-              {/* role: 'principal_investigator', name: '' } as Contact)*/}
               <LeadInvestigatorDropdown
                 token={token!}
                 orgMembership={orgMembership!}
@@ -174,8 +179,7 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
                 &nbsp; tab on the top right hand side.
               </Box>
             </Grid>
-
-            <Grid item xs={6}>
+            <Grid item xs={6} style={{ marginTop: '-24px' }}>
               <FormControl fullWidth>
                 <SimpleTextLabel htmlFor="affiliation">
                   Institutional Affiliation*
@@ -186,6 +190,7 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
                   onChange={e => {}}
                   id="affiliation"
                   multiline={false}
+                  className={classes.input}
                 />
               </FormControl>
             </Grid>
@@ -198,29 +203,33 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
                 <SimpleTextLabel htmlFor="notes">
                   What is your IRB of record?{' '}
                 </SimpleTextLabel>
-                <RadioGroup
-                  style={{ marginTop: '16px' }}
-                  aria-label="irb of record"
-                  name="irbOfRecord"
-                  value=""
-                  onChange={e => {}}
-                >
-                  <FormControlLabel
-                    control={<Radio value={'SAME'} />}
-                    label="Same Institutional Affiliation"
+                <Box pl={3} mt={1}>
+                  <RadioGroup
+                    style={{ marginTop: '16px' }}
+                    aria-label="irb of record"
+                    name="irbOfRecord"
+                    value=""
+                    onChange={e => {}}
+                  >
+                    <FormControlLabel
+                      control={<Radio value={'SAME'} />}
+                      label="Same Institutional Affiliation"
+                    />
+                    <FormControlLabel
+                      control={<Radio value={'OTHER'} />}
+                      label="Other:"
+                    />
+                  </RadioGroup>
+                  <SimpleTextInput
+                    value=""
+                    placeholder="Name IRB of record"
+                    onChange={e => {}}
+                    id="irbOfRecord"
+                    rows={5}
+                    className={classes.input}
+                    style={{ width: '100%' }}
                   />
-                  <FormControlLabel
-                    control={<Radio value={'OTHER'} />}
-                    label="Other:"
-                  />
-                </RadioGroup>
-                <SimpleTextInput
-                  value=""
-                  placeholder="Name IRB of record"
-                  onChange={e => {}}
-                  id="irbOfRecord"
-                  rows={5}
-                />
+                </Box>
               </FormControl>
             </Grid>
             <Grid item xs={6}></Grid>
@@ -235,62 +244,77 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
                   onChange={e => {}}
                   id="protocolId"
                   multiline={false}
+                  className={classes.input}
                 />
               </FormControl>
             </Grid>
             <Grid item xs={6}></Grid>
             <Grid item xs={6}>
               <MTBHeadingH2>IRB Decision: </MTBHeadingH2>
-              <FormControl>
-                <RadioGroup
-                  aria-label="Irb Decision"
-                  name="irbDecision"
-                  value=""
-                  onChange={e => {}}
-                >
-                  <FormControlLabel
-                    control={<Radio value={'APPROVED'} />}
-                    label="Approved"
-                    labelPlacement="end"
-                  />
-                  <div>
+              <Box pl={3} mt={2}>
+                <FormControl>
+                  <RadioGroup
+                    aria-label="Irb Decision"
+                    name="irbDecision"
+                    value=""
+                    onChange={e => {}}
+                  >
+                    <FormControlLabel
+                      control={<Radio value={'APPROVED'} />}
+                      label="Approved"
+                      labelPlacement="end"
+                    />
+                    <Box>
+                      <FormControl>
+                        <DatePicker
+                          label="Date of IRB Approval"
+                          id="approvalDate"
+                          value={null}
+                          onChange={e => {}}
+                        ></DatePicker>
+                      </FormControl>
+                      <FormControl>
+                        <DatePicker
+                          label="Date of Approval Expiration"
+                          id="expirationDate"
+                          value={null}
+                          onChange={e => {}}
+                        ></DatePicker>
+                      </FormControl>
+                    </Box>
+                    <FormControlLabel
+                      control={<Radio value={'EXEMPT'} />}
+                      label="Exempt"
+                    />
                     <FormControl>
                       <DatePicker
-                        label="Date of Approval"
-                        id="approvalDate"
+                        label="Date of Exemption"
+                        id="exemptionDate"
                         value={null}
                         onChange={e => {}}
                       ></DatePicker>
                     </FormControl>
-                    <FormControl>
-                      <DatePicker
-                        label="Date of Approval Expiration"
-                        id="expirationDate"
-                        value={null}
-                        onChange={e => {}}
-                      ></DatePicker>
-                    </FormControl>
-                  </div>
-                  <FormControlLabel
-                    control={<Radio value={'EXEMPT'} />}
-                    label="Exempt"
-                  />
-                  <FormControl>
-                    <DatePicker
-                      label="Date of Exemption"
-                      id="exemptionDate"
-                      value={null}
-                      onChange={e => {}}
-                    ></DatePicker>
-                  </FormControl>
-                </RadioGroup>
-              </FormControl>
+                  </RadioGroup>
+                </FormControl>
+              </Box>
             </Grid>
           </Grid>
-          <Box fontSize="18px">
-            Please note that you will <strong>no longer</strong> be able to{' '}
-            <strong>make changes</strong> to your study once you’ve{' '}
-            <strong>submit</strong> the information from this page.
+          <Box mt={2}>
+            <AlertWithText
+              severity="error"
+              icon={
+                <img
+                  src={Alert_Icon}
+                  style={{ height: '30px' }}
+                  alt={'study-warning'}
+                ></img>
+              }
+              style={{ fontSize: '18px', backgroundColor: 'transparent' }}
+            >
+              Please note that you will <strong>no longer</strong> be able to{' '}
+              <strong>make changes</strong> to your study once you’ve{' '}
+              <strong>submit</strong> the information from this page.
+            </AlertWithText>
           </Box>
         </Box>
       )}
