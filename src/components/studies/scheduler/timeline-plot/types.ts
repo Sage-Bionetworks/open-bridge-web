0fda1c8a3f1470e8237f0a99621de18395bb0b7c
Alina Gendel <alina.gendel@sagebase.org>
2021-08-09 13:09:23 -0700

    cleanup unused const

--- src/components/studies/scheduler/timeline-plot/types.ts
+++ src/components/studies/scheduler/timeline-plot/types.ts
@@ -14,28 +14,12 @@ export type TimelineScheduleItem = {
   assessments?: any[]
 }
 
-/*export const unitPixelWidth: Record<TimelineZoomLevel, number> = {
-  Daily: 1020,
-  Monthly: 35,
-  'Bi-Weekly': 80,
-  Weekly: 162,
-  Quarterly: 11,
-}*/
-
 export const unitPixelWidth: Record<TimelineZoomLevel, number> = {
   Daily: 1020,
   Monthly: 35,
   'Bi-Weekly': 80,
   Weekly: 102,
   Quarterly: 11,
-}
-
-export const unitPixelWidthBurst: Record<TimelineZoomLevel, number> = {
-  Daily: 1020,
-  Monthly: 28,
-  'Bi-Weekly': 63,
-  Weekly: 137,
-  Quarterly: 9,
 }
 
 export const daysPage: Record<TimelineZoomLevel, number> = {
