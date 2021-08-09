d2577ee57d8b228a450394f8275be8378e8d82cc
Leon Li <leonli3three@gmail.com>
2021-08-09 12:47:02 -0700

    Deleted text from IRB Details page

--- src/components/studies/launch/IrbDetails.tsx
+++ src/components/studies/launch/IrbDetails.tsx
@@ -584,23 +584,25 @@ const IrbDetails: React.FunctionComponent<IrbDetailsProps> = ({
               </Box>
             </Grid>
           </Grid>
-          <Box mt={2}>
-            <AlertWithText
-              severity="error"
-              icon={
-                <img
-                  src={Alert_Icon}
-                  style={{height: '30px'}}
-                  alt={'study-warning'}></img>
-              }
-              className={classes.alertText}>
-              Once your study is submitted, everything related to
-              sessions/scheduling/enrollment will be <strong>locked</strong>
-              -only <strong>Customize App</strong> and{' '}
-              <strong>Study & IRB</strong> details will be{' '}
-              <strong>editable.</strong>
-            </AlertWithText>
-          </Box>
+          {study.phase === 'in_flight' && (
+            <Box mt={2}>
+              <AlertWithText
+                severity="error"
+                icon={
+                  <img
+                    src={Alert_Icon}
+                    style={{height: '30px'}}
+                    alt={'study-warning'}></img>
+                }
+                className={classes.alertText}>
+                Once your study is submitted, everything related to
+                sessions/scheduling/enrollment will be <strong>locked</strong>
+                -only <strong>Customize App</strong> and{' '}
+                <strong>Study & IRB</strong> details will be{' '}
+                <strong>editable.</strong>
+              </AlertWithText>
+            </Box>
+          )}
         </Box>
       )}
       {isFinished && <LastScreen study={study}></LastScreen>}
