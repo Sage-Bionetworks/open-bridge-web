0d3ae089b9c9185dbcd14bf39f0808657348b20e
Leon Li <leonli3three@gmail.com>
2021-08-10 11:13:44 -0700

    Added recruitment as a phase in which to check if the AppDesign component should be read only.

--- src/components/studies/StudyBuilder.tsx
+++ src/components/studies/StudyBuilder.tsx
@@ -598,6 +598,7 @@ const StudyBuilder: FunctionComponent<StudyBuilderProps> = ({
                         <AppDesign
                           isReadOnly={
                             builderInfo.study.phase !== 'design' &&
+                            builderInfo.study.phase !== 'recruitment' &&
                             builderInfo.study.phase !== 'in_flight'
                           }
                           hasObjectChanged={hasObjectChanged}
