e86b2b1126f6cafd2f7d5b060bc9a2b6b40317b6
Alina Gendel <alina.gendel@sagebase.org>
2021-08-20 11:17:06 -0700

    add more path aliases

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,10 @@ module.exports = {
     alias: {
       '@components': path.resolve(__dirname, 'src/components/'),
       '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@helpers': path.resolve(__dirname, 'src/helpers/'),
+      '@services': path.resolve(__dirname, 'src/services/'),
+      '@typedefs': path.resolve(__dirname, 'src/types/'),
+      '@style': path.resolve(__dirname, 'src/style/'),
     },
   },
 }
