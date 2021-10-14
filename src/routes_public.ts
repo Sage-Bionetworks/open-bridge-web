aea181e109938fd08a019773a36c023683889360
Alina Gendel <alina.gendel@sagebase.org>
2021-10-14 09:28:25 -0700

    remove public routes without pages and make homepage a login page

--- src/routes_public.ts
+++ src/routes_public.ts
@@ -1,13 +1,16 @@
-import AccountCreate from './components/account/AccountCreate'
 import AssessmentDetail from './components/assessments/AssessmentDetail'
 import AssessmentLibrary from './components/assessments/AssessmentLibrary'
-import SessionsLayout from './components/layouts/sessions'
-import DevelopmentTeam from './components/static/DevelopmentTeam'
 import DownloadAppLandingPage from './components/static/DownloadAppLandingPage'
-import Plans from './components/static/Plans'
 import SignInPage from './SignInPage'
 
 export default [
+  {
+    path: '/',
+    name: '',
+    Component: SignInPage,
+    exact: true,
+    noToolbar: false,
+  },
   {
     path: '/app-store-download',
     name: '',
@@ -15,12 +18,12 @@ export default [
     exact: true,
     noToolbar: true,
   },
-  {
+  /*  {
     path: '/how-it-works',
     name: 'HOW IT WORKS',
     Component: DownloadAppLandingPage,
     exact: true,
-  },
+  },*/
   {
     path: '/assessments',
     name: 'ASSESSMENT LIBRARY',
@@ -33,7 +36,7 @@ export default [
     Component: AssessmentDetail,
     exact: true,
   },
-  {
+  /* {
     path: '/plans',
     name: 'PLANS',
     Component: Plans,
@@ -55,7 +58,7 @@ export default [
     name: 'CREATE ACCOUNT',
     Component: AccountCreate,
     isRhs: true,
-  },
+  },*/
   {
     path: '/sign-in',
     name: 'SIGN IN',
