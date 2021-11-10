2045e06b9af89672adb9407d12d72d92c1b50ab1
Alina Gendel <alina.gendel@sagebase.org>
2021-11-10 15:02:07 -0800

    create eventHooks

---
+++ src/components/studies/eventHooks.ts
@@ -0,0 +1,36 @@
+import {useUserSessionDataState} from '@helpers/AuthContext'
+import ScheduleService, {BurstEventObject} from '@services/schedule.service'
+import {ExtendedError} from '@typedefs/types'
+import {useQuery} from 'react-query'
+
+const EVENTS_KEYS = {
+  all: ['events'] as const,
+  list: (studyId: string | undefined) => [...EVENTS_KEYS.all, 'list'] as const,
+  // list: (filters: string) => [...todoKeys.lists(), { filters }] as const,
+  details: () => [...EVENTS_KEYS.all, 'detail'] as const,
+  detail: (id: string | undefined) => [...EVENTS_KEYS.details(), id] as const,
+}
+
+export const useEvents = (
+  studyId: string | undefined,
+  withBursts: boolean = true,
+  onlyCustom: boolean = true
+) => {
+  const {token} = useUserSessionDataState()
+
+  return useQuery<BurstEventObject[], ExtendedError>(
+    EVENTS_KEYS.list(studyId),
+    () =>
+      ScheduleService.getEventIdsForScheduleByStudyId(
+        studyId!,
+        token!,
+        onlyCustom,
+        withBursts
+      ),
+    {
+      enabled: !!studyId,
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  )
+}
