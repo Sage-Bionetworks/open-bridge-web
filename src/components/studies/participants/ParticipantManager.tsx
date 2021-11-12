b8bcd49b34b2fe10bab8c2191dcf441bd1886c3b
Alina Gendel <alina.gendel@sagebase.org>
2021-11-12 13:38:48 -0800

    break out styles from participant manager

--- src/components/studies/participants/ParticipantManager.tsx
+++ src/components/studies/participants/ParticipantManager.tsx
@@ -38,11 +38,10 @@ import {
   Tab,
   Tabs,
 } from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles'
 import EventService from '@services/event.service'
 import ParticipantService from '@services/participants.service'
 import StudyService from '@services/study.service'
-import {latoFont, poppinsFont, theme} from '@style/theme'
+import {theme} from '@style/theme'
 import constants from '@typedefs/constants'
 import {
   ExtendedError,
@@ -66,116 +65,10 @@ import ParticipantTableGrid from './grid/ParticipantTableGrid'
 import ParticipantTablePagination from './grid/ParticipantTablePagination'
 import BatchEditForm from './modify/BatchEditForm'
 import ParticipantManagerPlaceholder from './ParticipantManagerPlaceholder'
+import useStyles from './ParticipantManager_style'
 import ParticipantSearch from './ParticipantSearch'
 import ParticipantUtility from './participantUtility'
 import WithdrawnTabNoParticipantsPage from './WithdrawnTabNoParticipantsPage'
-
-const useStyles = makeStyles(theme => ({
-  root: {},
-  downloadPageLinkButton: {
-    position: 'absolute',
-    right: 0,
-    top: 0,
-    display: 'flex',
-    fontFamily: latoFont,
-    fontSize: '14px',
-  },
-
-  tab: {
-    marginRight: theme.spacing(2),
-    width: '250px',
-    clipPath: 'polygon(10% 0%, 90% 0, 98% 100%,0 100%)',
-    marginLeft: theme.spacing(-3.5),
-    zIndex: 0,
-    backgroundColor: '#F0F0F0',
-    fontSize: '12px',
-    fontFamily: poppinsFont,
-  },
-  gridToolBar: {
-    backgroundColor: theme.palette.common.white,
-    // padding: theme.spacing(1, 5, 0, 5),
-    height: theme.spacing(9),
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(3),
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-
-    '&  button': {
-      display: 'flex',
-      fontFamily: latoFont,
-      fontSize: '14px',
-    },
-  },
-
-  tabPanel: {
-    backgroundColor: theme.palette.common.white,
-    boxShadow: 'none',
-    padding: theme.spacing(0, 0, 2, 0),
-  },
-  studyId: {
-    color: '#393434',
-    marginRight: '24px',
-  },
-
-  topRow: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    fontSize: '16px',
-  },
-  horizontalGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  disabledImage: {
-    opacity: 0.5,
-  },
-  topRowImage: {
-    marginRight: theme.spacing(0.75),
-  },
-  deleteIcon: {
-    height: '17px',
-    width: '13px',
-  },
-  addParticipantIcon: {
-    width: theme.spacing(6),
-    height: theme.spacing(6),
-    backgroundColor: '#AEDCC9',
-    paddingTop: theme.spacing(1.5),
-  },
-  selectedTab: {
-    zIndex: 100,
-    backgroundColor: theme.palette.common.white,
-  },
-  withdrawnParticipants: {
-    width: '270px',
-  },
-  tab_icon: {
-    borderBottom: '1px solid transparent',
-  },
-  unactiveTabIcon: {
-    '&:hover div': {
-      borderBottom: '1px solid black',
-    },
-  },
-  collapsedAddTestUser: {
-    '& > rect': {
-      fill: '#AEDCC9',
-    },
-  },
-  primaryDialogButton: {
-    border: 'none',
-    height: '48px',
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-  },
-}))
 
 /** types and constants  */
 type ParticipantData = {
@@ -395,24 +288,6 @@ const ParticipantManager: FunctionComponent<ParticipantManagerProps> = () => {
     status: 'PENDING',
     data: null,
   })
-  /*
-  const {
-    data: scheduleEventIds,
-    run: getEvents,
-    status: eventsStatus,
-  } = useAsync<string[]>({
-    status: 'PENDING',
-    data: [],
-  })*/
-
-  /*React.useEffect(() => {
-    if (!study?.identifier) {
-      return
-    }
-    getEvents(
-      ScheduleService.getEventIdsForScheduleByStudyId(study.identifier, token!)
-    )
-  }, [study?.identifier, getEvents])*/
 
   React.useEffect(() => {
     if (!study?.identifier) {
