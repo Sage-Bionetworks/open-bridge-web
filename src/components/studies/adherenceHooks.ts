db9b7508fe226025b918243644a25f0f7680f903
Alina Gendel <alina.gendel@sagebase.org>
2021-12-22 11:10:30 -0800

    feat(adherence): add useAdherence hook

---
+++ src/components/studies/adherenceHooks.ts
@@ -0,0 +1,29 @@
+import {useUserSessionDataState} from '@helpers/AuthContext'
+import AdherenceService from '@services/adherence.service'
+import {EventStreamAdherenceReport, ExtendedError} from '@typedefs/types'
+import {useQuery} from 'react-query'
+
+const ADHERENCE_KEYS = {
+  all: ['adherence'] as const,
+  list: (studyId: string) => [...ADHERENCE_KEYS.all, 'list', studyId] as const,
+
+  detail: (studyId: string, userId: string) =>
+    [...ADHERENCE_KEYS.list(studyId), userId] as const,
+}
+
+export const useAdherence = (studyId: string, userId: string | undefined) => {
+  const {token} = useUserSessionDataState()
+  if (!userId) {
+    console.log('NEED TO DO')
+  }
+
+  return useQuery<EventStreamAdherenceReport, ExtendedError>(
+    ADHERENCE_KEYS.detail(studyId, userId!),
+    () => AdherenceService.getAdherenceForParticipant(studyId, userId!, token!),
+    {
+      enabled: !!studyId && !!userId && !!token,
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  )
+}
