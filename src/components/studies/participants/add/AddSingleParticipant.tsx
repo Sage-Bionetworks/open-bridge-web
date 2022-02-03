496f6e4ae16e54c5df91bc430e9975b8cbfc249e
Yongjoo Yoo <yyoo@w146.local>
2022-02-03 10:54:07 -0800

    refactor(participants): create hook using react-query to add single participants

--- src/components/studies/participants/add/AddSingleParticipant.tsx
+++ src/components/studies/participants/add/AddSingleParticipant.tsx
@@ -1,11 +1,11 @@
 // pick a date util library
 
+import {useAddParticipant} from '@components/studies/participantHooks'
 import {BlueButton} from '@components/widgets/StyledComponents'
 import Utility from '@helpers/utility'
 import {Box, CircularProgress} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import Alert from '@material-ui/lab/Alert'
-import ParticipantService from '@services/participants.service'
 import {ExtendedScheduleEventObject} from '@services/schedule.service'
 import {EditableParticipantData, ExtendedError, Phone} from '@typedefs/types'
 import React, {FunctionComponent} from 'react'
@@ -23,26 +23,6 @@ type AddSingleParticipantProps = {
   studyIdentifier: string
 }
 
-export async function addParticipantById(
-  studyIdentifier: string,
-  token: string,
-  options: EditableParticipantData
-) {
-  await ParticipantService.addParticipant(studyIdentifier, token, options)
-}
-
-export async function addParticipantByPhone(
-  studyIdentifier: string,
-  token: string,
-  phone: Phone,
-  options: EditableParticipantData
-) {
-  await ParticipantService.addParticipant(studyIdentifier, token, {
-    ...options,
-    phone,
-  })
-}
-
 // -----------------  Add participant  tab control
 const AddSingleParticipant: FunctionComponent<AddSingleParticipantProps> = ({
   onAdded,
@@ -58,40 +38,19 @@ const AddSingleParticipant: FunctionComponent<AddSingleParticipantProps> = ({
     }
   )
   const [error, setError] = React.useState('')
-  const [isLoading, setIsLoading] = React.useState(false)
 
-  const addSingleParticipant = async (participant: EditableParticipantData) => {
-    setError('')
-    setIsLoading(true)
+  const {isLoading, mutateAsync} = useAddParticipant()
+  const onAddParticipant = async(participant: EditableParticipantData) => {
     let options: EditableParticipantData = {
       externalId: participant.externalId,
       events: participant.events,
       clientTimeZone: participant.clientTimeZone,
       note: participant.note,
     }
-
-    try {
-      if (!isEnrolledById) {
-        await addParticipantByPhone(
-          studyIdentifier,
-          token,
-          Utility.makePhone(participant.phoneNumber || ''),
-          options
-        )
-      } else {
-        await addParticipantById(studyIdentifier, token, options)
-      }
-
-      onAdded()
-      setParticipant({externalId: ''})
-    } catch (e) {
-      setError(
-        (e as ExtendedError).message.toString() ||
-          (e as ExtendedError).toString()
-      )
-    } finally {
-      setIsLoading(false)
-    }
+    isEnrolledById 
+    ? mutateAsync({studyId:studyIdentifier,options}) 
+    : mutateAsync({studyId:studyIdentifier,options,phone:Utility.makePhone(participant.phoneNumber || '')}) 
+    setParticipant({externalId: ''})
   }
 
   const isAddDisabled = (): boolean => {
@@ -125,7 +84,8 @@ const AddSingleParticipant: FunctionComponent<AddSingleParticipantProps> = ({
           color="primary"
           variant="contained"
           disabled={isAddDisabled()}
-          onClick={() => addSingleParticipant(participant)}>
+          onClick={() => onAddParticipant(participant)}>
+
           +Add to study
         </BlueButton>
       </Box>
