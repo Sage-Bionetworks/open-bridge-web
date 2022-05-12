5c8fa08af6695074b3a4935fbe6d62e617615698
Alina Gendel <alina.gendel@sagebase.org>
2022-05-11 22:19:56 -0700

    chore(core): remove StudyInfoContext

--- src/AuthenticatedApp.tsx
+++ src/AuthenticatedApp.tsx
@@ -1,35 +1,38 @@
-import {StudySection} from '@components/studies/sections'
-import {useStudy} from '@components/studies/studyHooks'
-import {useUserSessionDataState} from '@helpers/AuthContext'
-import {useStudyInfoDataDispatch} from '@helpers/StudyInfoContext'
-import React, {FunctionComponent, ReactNode} from 'react'
-import {useErrorHandler} from 'react-error-boundary'
+import { StudySection } from '@components/studies/sections'
+import { useStudy } from '@components/studies/studyHooks'
+import SurveyTopNav from '@components/surveys/SurveyTopNav'
+import { useUserSessionDataState } from '@helpers/AuthContext'
+import React, { FunctionComponent, ReactNode } from 'react'
+import { useErrorHandler } from 'react-error-boundary'
 import {
   Route,
   RouteComponentProps,
   Switch,
   useLocation,
   useParams,
-  withRouter,
+  withRouter
 } from 'react-router-dom'
 import StudyTopNav from './components/studies/StudyTopNav'
 import TopNav from './components/widgets/AppTopNav'
 import Utility from './helpers/utility'
 import PrivateRoutes from './routes_private'
-import {UserSessionData} from './types/types'
+import { UserSessionData } from './types/types'
+
 
 const Wrapper: FunctionComponent<
-  RouteComponentProps & {sessionData: UserSessionData; children: ReactNode}
-> = ({children, sessionData}) => {
-  let {id: studyId, section: studySection} = useParams<{
+  RouteComponentProps & { sessionData: UserSessionData; children: ReactNode }
+> = ({ children, sessionData }) => {
+  let { id: studyId, section: studySection } = useParams<{
     id: string
     section: StudySection
   }>()
   //only use studyId in study builder or
   const notStudyId = useLocation().pathname.includes(`/assessments/${studyId}`)
-  const {data: study, error} = useStudy(notStudyId ? undefined : studyId)
+  const isSurveyPath = useLocation().pathname.includes(`/surveys`)
+  console.log(isSurveyPath)
+  const { data: study, error } = useStudy(notStudyId ? undefined : studyId)
   const handleError = useErrorHandler()
-  const studyDataUpdateFn = useStudyInfoDataDispatch()
+
   if (error) {
     handleError(error)
   }
@@ -37,27 +40,24 @@ const Wrapper: FunctionComponent<
   React.useEffect(() => {
     Utility.setBodyClass(studySection)
   }, [studySection])
-  React.useEffect(() => {
-    if (study) {
-      studyDataUpdateFn({type: 'SET_STUDY', payload: {study}})
-    }
-  }, [study, studyDataUpdateFn])
+
 
   return (
     <>
-      {!study && (
-        <TopNav
-          routes={PrivateRoutes}
-          sessionData={sessionData}
-          appId={sessionData.appId}
-        />
-      )}
-      {study && (
-        <StudyTopNav
-          study={study}
-          error={error}
-          currentSection={studySection}></StudyTopNav>
-      )}
+      {isSurveyPath ? <SurveyTopNav></SurveyTopNav> :
+
+        (!study ? (
+          <TopNav
+            routes={PrivateRoutes}
+            sessionData={sessionData}
+            appId={sessionData.appId}
+          />
+        ) : (
+          <StudyTopNav
+            study={study}
+            error={error}
+            currentSection={studySection}></StudyTopNav>
+        ))}
       <main>{children}</main>
     </>
   )
@@ -67,8 +67,8 @@ const AuthenticatedApp: FunctionComponent<
   {
     sessionData: UserSessionData
   } & RouteComponentProps
-> = ({sessionData, location, match}) => {
-  const {token} = useUserSessionDataState()
+> = ({ sessionData, location, match }) => {
+  const { token } = useUserSessionDataState()
 
   if (!token) {
     //save location and redirect
@@ -80,7 +80,7 @@ const AuthenticatedApp: FunctionComponent<
   return (
     <>
       <Switch>
-        {PrivateRoutes.map(({path, Component}, key) => (
+        {PrivateRoutes.map(({ path, Component }, key) => (
           <Route
             exact
             path={path}
---
+++ src/components/surveys/SurveyTopNav.tsx
@@ -0,0 +1,304 @@
+import ParticipantsIcon from '@assets/group_participants_icon.svg'
+import Logo from '@assets/logo_mtb.svg'
+import BreadCrumb from '@components/widgets/BreadCrumb'
+import MobileDrawerMenuHeader from '@components/widgets/MobileDrawerMenuHeader'
+import Utility from '@helpers/utility'
+import MenuIcon from '@mui/icons-material/Menu'
+import PeopleIcon from '@mui/icons-material/People'
+import {
+  Alert,
+  Box,
+  Drawer,
+  Hidden,
+  IconButton
+} from '@mui/material'
+import Toolbar from '@mui/material/Toolbar'
+import makeStyles from '@mui/styles/makeStyles'
+import { latoFont } from '@style/theme'
+import constants from '@typedefs/constants'
+import { ExtendedError } from '@typedefs/types'
+import clsx from 'clsx'
+import React, { FunctionComponent } from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const useStyles = makeStyles(theme => ({
+  rootSurveyTopNav: {
+    backgroundColor: '#F7F7F7',
+    padding: theme.spacing(0),
+    borderBottom: `1px solid #DFDFDF`,
+  },
+  logo: {
+    '&:hover': {
+      opacity: 0.7,
+    },
+  },
+  toolbarStudyHeader: {
+    padding: theme.spacing(2.5, 5, 0, 5),
+    height: theme.spacing(9),
+    display: 'flex',
+    backgroundColor: '#F7F7F7', //'#F8F8F8',
+
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  toolbar: {
+    alignItems: 'center',
+    minHeight: 'auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    '&:last-child': {
+      paddingRight: 0,
+    },
+  },
+
+  selectedLink: {
+    borderBottom: '4px solid black',
+    paddingBottom: theme.spacing(2),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    float: 'right',
+    '&::after': {
+      content: '',
+      display: 'table',
+      clear: 'both',
+    },
+  },
+  drawer: {
+    width: '320px',
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: '320px',
+
+    '& $toolbarLink': {
+      flextGroup: 0,
+      padding: theme.spacing(1, 2),
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+    },
+
+    '& $toolbarLink:first-child': {
+      paddingLeft: '8px',
+    },
+
+    '& $selectedLink': {
+      border: 'none',
+      fontWeight: 'bolder',
+    },
+    backgroundColor: '#F8F8F8',
+  },
+  toolbarLink: {
+    padding: theme.spacing(2),
+    flexGrow: 1,
+    fontFamily: latoFont,
+    fontSize: '15px',
+
+    textDecoration: 'none',
+    color: 'inherit',
+    flexShrink: 0,
+
+    '&:first-child': {
+      // paddingLeft: theme.spacing(0.5),
+    },
+    '&:last-child': {
+      paddingRight: theme.spacing(0.5),
+      paddingLeft: theme.spacing(0.5),
+    },
+    '&$selectedLink': {
+      paddingTop: '20px',
+    },
+  },
+  mobileToolBarLink: {
+    fontFamily: latoFont,
+    fontSize: '15px',
+    textDecoration: 'none',
+    color: 'inherit',
+    flexShrink: 0,
+    height: '56px',
+    boxSizing: 'border-box',
+    paddingLeft: theme.spacing(3),
+    '&:hover': {
+      backgroundColor: '#fff',
+    },
+    display: 'flex',
+    alignItems: 'center',
+    borderLeft: '4px solid transparent',
+  },
+  mobileSelectedLink: {
+    borderLeft: '4px solid #353535',
+    fontWeight: 'bolder',
+  },
+  blackXIcon: {
+    width: '16px',
+    height: '16px',
+  },
+  accessSettingsDrawerOption: {
+    display: 'flex',
+    marginTop: theme.spacing(4),
+  },
+}))
+
+type SurveyTopNavProps = {
+  surveyId?: string
+  error?: ExtendedError
+
+}
+
+const SurveyTopNav: FunctionComponent<SurveyTopNavProps> = ({
+  surveyId,
+  error
+}: SurveyTopNavProps) => {
+
+  const [isMobileOpen, setIsMobileOpen] = React.useState(false)
+  const classes = useStyles()
+
+  const links = [{ name: 'SURVEY DESIGN', path: '/surveys' }, { name: 'BRANCHING LOGIC', path: '/surveys/branching' }]
+
+
+  return (
+    <Box className={classes.rootSurveyTopNav}>
+      SURVEYPP
+      <Hidden lgUp>
+        <IconButton
+          color="inherit"
+          aria-label="Open drawer"
+          edge="end"
+          onClick={() => setIsMobileOpen(!isMobileOpen)}
+          className={classes.menuButton}
+          size="large">
+          <MenuIcon></MenuIcon>
+        </IconButton>
+      </Hidden>
+      <Hidden lgDown>
+        <Box className={classes.toolbarStudyHeader}>
+          <Toolbar
+            component="nav"
+            variant="dense"
+            disableGutters={true}
+            className={classes.toolbar}
+            style={{
+              paddingTop: '0',
+              alignItems: 'center',
+            }}>
+            <NavLink
+              to="/studies"
+              key="/studies"
+              className={classes.toolbarLink}
+              style={{ padding: '0 24px 0 0' }}>
+              <img
+                src={Logo}
+                className={classes.logo}
+                key="img_home"
+                alt="home"
+              />
+            </NavLink>
+
+            <BreadCrumb
+              links={[{ url: '/', text: '' }]}
+            ></BreadCrumb>
+
+
+
+          </Toolbar>
+          <Toolbar className={classes.toolbar}>
+            {links
+              .filter(section => section.name)
+              .map(section => (<NavLink
+                to={section.path.replace(':id', surveyId ?? '')}
+                key={section.path}
+                className={classes.toolbarLink}
+                activeClassName={classes.selectedLink}>
+                {section.name}
+              </NavLink>)
+
+              )}
+          </Toolbar>
+          <Toolbar
+            className={classes.toolbar}
+            style={{ width: '160px', overflow: 'hidden' }}>
+            {(Utility.isInAdminRole() || true) /* enable all aggess*/ && (
+              <NavLink
+                to={constants.restrictedPaths.ACCESS_SETTINGS.replace(
+                  ':id',
+                  surveyId ?? ''
+                )}
+                key={'path-to-access-settings'}
+                className={classes.toolbarLink}
+                activeClassName={classes.selectedLink}
+                style={{ display: 'flex' }}>
+                <PeopleIcon></PeopleIcon>&nbsp;&nbsp;Access settings
+              </NavLink>
+            )}
+          </Toolbar>
+        </Box>
+      </Hidden>
+      <nav className={classes.drawer}>
+        <Drawer
+          variant="temporary"
+          anchor="right"
+          open={isMobileOpen}
+          onClose={() => setIsMobileOpen(false)}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}>
+          <MobileDrawerMenuHeader
+            setIsMobileOpen={setIsMobileOpen}
+            type="IN_STUDY"></MobileDrawerMenuHeader>
+          {links
+            .filter(section => section.name)
+            .map(section =>
+
+              <NavLink
+                to={section.path.replace(':id', surveyId ?? '')}
+                key={section.path}
+                className={classes.mobileToolBarLink}
+                activeClassName={classes.mobileSelectedLink}
+                onClick={() => setIsMobileOpen(false)}>
+                {section.name}
+              </NavLink>
+
+            )}
+          <NavLink
+            to={constants.restrictedPaths.ACCESS_SETTINGS.replace(
+              ':id',
+              surveyId ?? ''
+            )}
+            key={'path-to-access-settings'}
+            className={clsx(
+              classes.mobileToolBarLink,
+              classes.accessSettingsDrawerOption
+            )}
+            activeClassName={classes.mobileSelectedLink}
+            onClick={() => setIsMobileOpen(false)}>
+            <img src={ParticipantsIcon} style={{ marginRight: '20px' }}></img>
+            Access settings
+          </NavLink>
+        </Drawer>
+      </nav>
+
+      {error && (
+        <Box mx="auto" textAlign="center">
+          <Alert
+            variant="outlined"
+            color="error"
+            style={{ marginBottom: '10px' }}>
+            {' '}
+            {error.statusCode}
+            You do not have the permission to access this feature. Please
+            contact your study administrator
+          </Alert>
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+export default SurveyTopNav
