de07c104c320bd4c07bd5a2c9ca9627595ebf11b
Alina Gendel <alina.gendel@sagebase.org>
2022-08-08 09:37:26 -0700

    sessionIcon (style): fix the case

--- src/components/widgets/SessionIcon.tsx
+++ src/components/widgets/SessionIcon.tsx
@@ -1,103 +1,103 @@
-import {ReactComponent as _01c} from '@assets/symbols/01_c.svg'
-import {ReactComponent as _01iR} from '@assets/symbols/01_i-1.svg'
-import {ReactComponent as _01i} from '@assets/symbols/01_i.svg'
-import {ReactComponent as _01pR} from '@assets/symbols/01_p-1.svg'
-import {ReactComponent as _01p} from '@assets/symbols/01_p.svg'
-import {ReactComponent as _02c} from '@assets/symbols/02_c.svg'
-import {ReactComponent as _02iR} from '@assets/symbols/02_i-1.svg'
-import {ReactComponent as _02i} from '@assets/symbols/02_i.svg'
-import {ReactComponent as _02pR} from '@assets/symbols/02_p-1.svg'
-import {ReactComponent as _02p} from '@assets/symbols/02_p.svg'
-import {ReactComponent as _03c} from '@assets/symbols/03_c.svg'
-import {ReactComponent as _03iR} from '@assets/symbols/03_i-1.svg'
-import {ReactComponent as _03i} from '@assets/symbols/03_i.svg'
-import {ReactComponent as _03pR} from '@assets/symbols/03_p-1.svg'
-import {ReactComponent as _03p} from '@assets/symbols/03_p.svg'
-import {ReactComponent as _04c} from '@assets/symbols/04_c.svg'
-import {ReactComponent as _04iR} from '@assets/symbols/04_i-1.svg'
-import {ReactComponent as _04i} from '@assets/symbols/04_i.svg'
-import {ReactComponent as _04pR} from '@assets/symbols/04_p-1.svg'
-import {ReactComponent as _04p} from '@assets/symbols/04_p.svg'
-import {ReactComponent as _05c} from '@assets/symbols/05_c.svg'
-import {ReactComponent as _05iR} from '@assets/symbols/05_i-1.svg'
-import {ReactComponent as _05i} from '@assets/symbols/05_i.svg'
-import {ReactComponent as _05pR} from '@assets/symbols/05_p-1.svg'
-import {ReactComponent as _05p} from '@assets/symbols/05_p.svg'
-import {ReactComponent as _06c} from '@assets/symbols/06_c.svg'
-import {ReactComponent as _06iR} from '@assets/symbols/06_i-1.svg'
-import {ReactComponent as _06i} from '@assets/symbols/06_i.svg'
-import {ReactComponent as _06pR} from '@assets/symbols/06_p-1.svg'
-import {ReactComponent as _06p} from '@assets/symbols/06_p.svg'
-import {ReactComponent as _07c} from '@assets/symbols/07_c.svg'
-import {ReactComponent as _07iR} from '@assets/symbols/07_i-1.svg'
-import {ReactComponent as _07i} from '@assets/symbols/07_i.svg'
-import {ReactComponent as _07pR} from '@assets/symbols/07_p-1.svg'
-import {ReactComponent as _07p} from '@assets/symbols/07_p.svg'
-import {ReactComponent as _08c} from '@assets/symbols/08_c.svg'
-import {ReactComponent as _08iR} from '@assets/symbols/08_i-1.svg'
-import {ReactComponent as _08i} from '@assets/symbols/08_i.svg'
-import {ReactComponent as _08pR} from '@assets/symbols/08_p-1.svg'
-import {ReactComponent as _08p} from '@assets/symbols/08_p.svg'
-import {ReactComponent as _09c} from '@assets/symbols/09_c.svg'
-import {ReactComponent as _09iR} from '@assets/symbols/09_i-1.svg'
-import {ReactComponent as _09i} from '@assets/symbols/09_i.svg'
-import {ReactComponent as _09pR} from '@assets/symbols/09_p-1.svg'
-import {ReactComponent as _09p} from '@assets/symbols/09_p.svg'
-import {ReactComponent as _10c} from '@assets/symbols/10_c.svg'
-import {ReactComponent as _10iR} from '@assets/symbols/10_i-1.svg'
-import {ReactComponent as _10i} from '@assets/symbols/10_i.svg'
-import {ReactComponent as _10pR} from '@assets/symbols/10_p-1.svg'
-import {ReactComponent as _10p} from '@assets/symbols/10_p.svg'
-import {ReactComponent as _11c} from '@assets/symbols/11_c.svg'
-import {ReactComponent as _11iR} from '@assets/symbols/11_i-1.svg'
-import {ReactComponent as _11i} from '@assets/symbols/11_i.svg'
-import {ReactComponent as _11pR} from '@assets/symbols/11_p-1.svg'
-import {ReactComponent as _11p} from '@assets/symbols/11_p.svg'
-import {ReactComponent as _12c} from '@assets/symbols/12_c.svg'
-import {ReactComponent as _12iR} from '@assets/symbols/12_i-1.svg'
-import {ReactComponent as _12i} from '@assets/symbols/12_i.svg'
-import {ReactComponent as _12pR} from '@assets/symbols/12_p-1.svg'
-import {ReactComponent as _12p} from '@assets/symbols/12_p.svg'
-import {ReactComponent as _13c} from '@assets/symbols/13_c.svg'
-import {ReactComponent as _13iR} from '@assets/symbols/13_i-1.svg'
-import {ReactComponent as _13i} from '@assets/symbols/13_i.svg'
-import {ReactComponent as _13pR} from '@assets/symbols/13_p-1.svg'
-import {ReactComponent as _13p} from '@assets/symbols/13_p.svg'
-import {ReactComponent as _14c} from '@assets/symbols/14_c.svg'
-import {ReactComponent as _14iR} from '@assets/symbols/14_i-1.svg'
-import {ReactComponent as _14i} from '@assets/symbols/14_i.svg'
-import {ReactComponent as _14pR} from '@assets/symbols/14_p-1.svg'
-import {ReactComponent as _14p} from '@assets/symbols/14_p.svg'
-import {ReactComponent as _15c} from '@assets/symbols/15_c.svg'
-import {ReactComponent as _15iR} from '@assets/symbols/15_i-1.svg'
-import {ReactComponent as _15i} from '@assets/symbols/15_i.svg'
-import {ReactComponent as _15pR} from '@assets/symbols/15_p-1.svg'
-import {ReactComponent as _15p} from '@assets/symbols/15_p.svg'
-import {ReactComponent as _16c} from '@assets/symbols/16_c.svg'
-import {ReactComponent as _16iR} from '@assets/symbols/16_i-1.svg'
-import {ReactComponent as _16i} from '@assets/symbols/16_i.svg'
-import {ReactComponent as _16pR} from '@assets/symbols/16_p-1.svg'
-import {ReactComponent as _16p} from '@assets/symbols/16_p.svg'
-import {ReactComponent as _17c} from '@assets/symbols/17_c.svg'
-import {ReactComponent as _17iR} from '@assets/symbols/17_i-1.svg'
-import {ReactComponent as _17i} from '@assets/symbols/17_i.svg'
-import {ReactComponent as _17pR} from '@assets/symbols/17_p-1.svg'
-import {ReactComponent as _17p} from '@assets/symbols/17_p.svg'
-import {ReactComponent as _18c} from '@assets/symbols/18_c.svg'
-import {ReactComponent as _18iR} from '@assets/symbols/18_i-1.svg'
-import {ReactComponent as _18i} from '@assets/symbols/18_i.svg'
-import {ReactComponent as _18pR} from '@assets/symbols/18_p-1.svg'
-import {ReactComponent as _18p} from '@assets/symbols/18_p.svg'
-import {ReactComponent as _19c} from '@assets/symbols/19_c.svg'
-import {ReactComponent as _19iR} from '@assets/symbols/19_i-1.svg'
-import {ReactComponent as _19i} from '@assets/symbols/19_i.svg'
-import {ReactComponent as _19pR} from '@assets/symbols/19_p-1.svg'
-import {ReactComponent as _19p} from '@assets/symbols/19_p.svg'
-import {ReactComponent as _20c} from '@assets/symbols/20_c.svg'
-import {ReactComponent as _20iR} from '@assets/symbols/20_i-1.svg'
-import {ReactComponent as _20i} from '@assets/symbols/20_i.svg'
-import {ReactComponent as _20pR} from '@assets/symbols/20_p-1.svg'
-import {ReactComponent as _20p} from '@assets/symbols/20_p.svg'
+import {ReactComponent as S01c} from '@assets/symbols/01_c.svg'
+import {ReactComponent as S01iR} from '@assets/symbols/01_i-1.svg'
+import {ReactComponent as S01i} from '@assets/symbols/01_i.svg'
+import {ReactComponent as S01pR} from '@assets/symbols/01_p-1.svg'
+import {ReactComponent as S01p} from '@assets/symbols/01_p.svg'
+import {ReactComponent as S02c} from '@assets/symbols/02_c.svg'
+import {ReactComponent as S02iR} from '@assets/symbols/02_i-1.svg'
+import {ReactComponent as S02i} from '@assets/symbols/02_i.svg'
+import {ReactComponent as S02pR} from '@assets/symbols/02_p-1.svg'
+import {ReactComponent as S02p} from '@assets/symbols/02_p.svg'
+import {ReactComponent as S03c} from '@assets/symbols/03_c.svg'
+import {ReactComponent as S03iR} from '@assets/symbols/03_i-1.svg'
+import {ReactComponent as S03i} from '@assets/symbols/03_i.svg'
+import {ReactComponent as S03pR} from '@assets/symbols/03_p-1.svg'
+import {ReactComponent as S03p} from '@assets/symbols/03_p.svg'
+import {ReactComponent as S04c} from '@assets/symbols/04_c.svg'
+import {ReactComponent as S04iR} from '@assets/symbols/04_i-1.svg'
+import {ReactComponent as S04i} from '@assets/symbols/04_i.svg'
+import {ReactComponent as S04pR} from '@assets/symbols/04_p-1.svg'
+import {ReactComponent as S04p} from '@assets/symbols/04_p.svg'
+import {ReactComponent as S05c} from '@assets/symbols/05_c.svg'
+import {ReactComponent as S05iR} from '@assets/symbols/05_i-1.svg'
+import {ReactComponent as S05i} from '@assets/symbols/05_i.svg'
+import {ReactComponent as S05pR} from '@assets/symbols/05_p-1.svg'
+import {ReactComponent as S05p} from '@assets/symbols/05_p.svg'
+import {ReactComponent as S06c} from '@assets/symbols/06_c.svg'
+import {ReactComponent as S06iR} from '@assets/symbols/06_i-1.svg'
+import {ReactComponent as S06i} from '@assets/symbols/06_i.svg'
+import {ReactComponent as S06pR} from '@assets/symbols/06_p-1.svg'
+import {ReactComponent as S06p} from '@assets/symbols/06_p.svg'
+import {ReactComponent as S07c} from '@assets/symbols/07_c.svg'
+import {ReactComponent as S07iR} from '@assets/symbols/07_i-1.svg'
+import {ReactComponent as S07i} from '@assets/symbols/07_i.svg'
+import {ReactComponent as S07pR} from '@assets/symbols/07_p-1.svg'
+import {ReactComponent as S07p} from '@assets/symbols/07_p.svg'
+import {ReactComponent as S08c} from '@assets/symbols/08_c.svg'
+import {ReactComponent as S08iR} from '@assets/symbols/08_i-1.svg'
+import {ReactComponent as S08i} from '@assets/symbols/08_i.svg'
+import {ReactComponent as S08pR} from '@assets/symbols/08_p-1.svg'
+import {ReactComponent as S08p} from '@assets/symbols/08_p.svg'
+import {ReactComponent as S09c} from '@assets/symbols/09_c.svg'
+import {ReactComponent as S09iR} from '@assets/symbols/09_i-1.svg'
+import {ReactComponent as S09i} from '@assets/symbols/09_i.svg'
+import {ReactComponent as S09pR} from '@assets/symbols/09_p-1.svg'
+import {ReactComponent as S09p} from '@assets/symbols/09_p.svg'
+import {ReactComponent as S10c} from '@assets/symbols/10_c.svg'
+import {ReactComponent as S10iR} from '@assets/symbols/10_i-1.svg'
+import {ReactComponent as S10i} from '@assets/symbols/10_i.svg'
+import {ReactComponent as S10pR} from '@assets/symbols/10_p-1.svg'
+import {ReactComponent as S10p} from '@assets/symbols/10_p.svg'
+import {ReactComponent as S11c} from '@assets/symbols/11_c.svg'
+import {ReactComponent as S11iR} from '@assets/symbols/11_i-1.svg'
+import {ReactComponent as S11i} from '@assets/symbols/11_i.svg'
+import {ReactComponent as S11pR} from '@assets/symbols/11_p-1.svg'
+import {ReactComponent as S11p} from '@assets/symbols/11_p.svg'
+import {ReactComponent as S12c} from '@assets/symbols/12_c.svg'
+import {ReactComponent as S12iR} from '@assets/symbols/12_i-1.svg'
+import {ReactComponent as S12i} from '@assets/symbols/12_i.svg'
+import {ReactComponent as S12pR} from '@assets/symbols/12_p-1.svg'
+import {ReactComponent as S12p} from '@assets/symbols/12_p.svg'
+import {ReactComponent as S13c} from '@assets/symbols/13_c.svg'
+import {ReactComponent as S13iR} from '@assets/symbols/13_i-1.svg'
+import {ReactComponent as S13i} from '@assets/symbols/13_i.svg'
+import {ReactComponent as S13pR} from '@assets/symbols/13_p-1.svg'
+import {ReactComponent as S13p} from '@assets/symbols/13_p.svg'
+import {ReactComponent as S14c} from '@assets/symbols/14_c.svg'
+import {ReactComponent as S14iR} from '@assets/symbols/14_i-1.svg'
+import {ReactComponent as S14i} from '@assets/symbols/14_i.svg'
+import {ReactComponent as S14pR} from '@assets/symbols/14_p-1.svg'
+import {ReactComponent as S14p} from '@assets/symbols/14_p.svg'
+import {ReactComponent as S15c} from '@assets/symbols/15_c.svg'
+import {ReactComponent as S15iR} from '@assets/symbols/15_i-1.svg'
+import {ReactComponent as S15i} from '@assets/symbols/15_i.svg'
+import {ReactComponent as S15pR} from '@assets/symbols/15_p-1.svg'
+import {ReactComponent as S15p} from '@assets/symbols/15_p.svg'
+import {ReactComponent as S16c} from '@assets/symbols/16_c.svg'
+import {ReactComponent as S16iR} from '@assets/symbols/16_i-1.svg'
+import {ReactComponent as S16i} from '@assets/symbols/16_i.svg'
+import {ReactComponent as S16pR} from '@assets/symbols/16_p-1.svg'
+import {ReactComponent as S16p} from '@assets/symbols/16_p.svg'
+import {ReactComponent as S17c} from '@assets/symbols/17_c.svg'
+import {ReactComponent as S17iR} from '@assets/symbols/17_i-1.svg'
+import {ReactComponent as S17i} from '@assets/symbols/17_i.svg'
+import {ReactComponent as S17pR} from '@assets/symbols/17_p-1.svg'
+import {ReactComponent as S17p} from '@assets/symbols/17_p.svg'
+import {ReactComponent as S18c} from '@assets/symbols/18_c.svg'
+import {ReactComponent as S18iR} from '@assets/symbols/18_i-1.svg'
+import {ReactComponent as S18i} from '@assets/symbols/18_i.svg'
+import {ReactComponent as S18pR} from '@assets/symbols/18_p-1.svg'
+import {ReactComponent as S18p} from '@assets/symbols/18_p.svg'
+import {ReactComponent as S19c} from '@assets/symbols/19_c.svg'
+import {ReactComponent as S19iR} from '@assets/symbols/19_i-1.svg'
+import {ReactComponent as S19i} from '@assets/symbols/19_i.svg'
+import {ReactComponent as S19pR} from '@assets/symbols/19_p-1.svg'
+import {ReactComponent as S19p} from '@assets/symbols/19_p.svg'
+import {ReactComponent as S20c} from '@assets/symbols/20_c.svg'
+import {ReactComponent as S20iR} from '@assets/symbols/20_i-1.svg'
+import {ReactComponent as S20i} from '@assets/symbols/20_i.svg'
+import {ReactComponent as S20pR} from '@assets/symbols/20_p-1.svg'
+import {ReactComponent as S20p} from '@assets/symbols/20_p.svg'
 import makeStyles from '@mui/styles/makeStyles'
 import React from 'react'
 
@@ -114,69 +114,69 @@ const useStyles = makeStyles({
 })
 
 const SessionSymbolsArray: any[] = [
-  <_01c></_01c>,
-  <_02c></_02c>,
-  <_03c></_03c>,
-  <_04c></_04c>,
-  <_05c></_05c>,
-  <_06c></_06c>,
-  <_07c></_07c>,
-  <_08c></_08c>,
-  <_09c></_09c>,
-  <_10c></_10c>,
-  <_11c></_11c>,
-  <_12c></_12c>,
-  <_13c></_13c>,
-  <_14c></_14c>,
-  <_15c></_15c>,
-  <_16c></_16c>,
-  <_17c></_17c>,
-  <_18c></_18c>,
-  <_19c></_19c>,
-  <_20c></_20c>,
+  <S01c></S01c>,
+  <S02c> </S02c>,
+  <S03c> </S03c>,
+  <S04c> </S04c>,
+  <S05c> </S05c>,
+  <S06c> </S06c>,
+  <S07c> </S07c>,
+  <S08c> </S08c>,
+  <S09c> </S09c>,
+  <S10c> </S10c>,
+  <S11c> </S11c>,
+  <S12c> </S12c>,
+  <S13c> </S13c>,
+  <S14c> </S14c>,
+  <S15c> </S15c>,
+  <S16c> </S16c>,
+  <S17c> </S17c>,
+  <S18c> </S18c>,
+  <S19c> </S19c>,
+  <S20c> </S20c>,
 ]
 
 export const SessionSymbols = new Map([
-  ['Session1Circle', [<_01c />, <_01p />, <_01i />, <_01pR />, <_01iR />]],
-  ['Session2Triangle', [<_02c />, <_02p />, <_02i />, <_02pR />, <_02iR />]],
-  ['Session3Square', [<_03c />, <_03p />, <_03i />, <_03pR />, <_03iR />]],
-  ['Session4Diamond', [<_04c />, <_04p />, <_04i />, <_04pR />, <_04iR />]],
-  ['Session5Pentagon', [<_05c />, <_05p />, <_05i />, <_05pR />, <_05iR />]],
-  ['Session6Hexagon', [<_06c />, <_06p />, <_06i />, <_06pR />, <_06iR />]],
+  ['Session1Circle', [<S01c />, <S01p />, <S01i />, <S01pR />, <S01iR />]],
+  ['Session2Triangle', [<S02c />, <S02p />, <S02i />, <S02pR />, <S02iR />]],
+  ['Session3Square', [<S03c />, <S03p />, <S03i />, <S03pR />, <S03iR />]],
+  ['Session4Diamond', [<S04c />, <S04p />, <S04i />, <S04pR />, <S04iR />]],
+  ['Session5Pentagon', [<S05c />, <S05p />, <S05i />, <S05pR />, <S05iR />]],
+  ['Session6Hexagon', [<S06c />, <S06p />, <S06i />, <S06pR />, <S06iR />]],
   [
     'Session7TriangleSharpUp',
-    [<_07c />, <_07p />, <_07i />, <_07pR />, <_07iR />],
+    [<S07c />, <S07p />, <S07i />, <S07pR />, <S07iR />],
   ],
-  ['Session8SquareSharp', [<_08c />, <_08p />, <_08i />, <_08pR />, <_08iR />]],
+  ['Session8SquareSharp', [<S08c />, <S08p />, <S08i />, <S08pR />, <S08iR />]],
   [
     'Session9DiamondSharp',
-    [<_09c />, <_09p />, <_09i />, <_09pR />, <_09iR />],
+    [<S09c />, <S09p />, <S09i />, <S09pR />, <S09iR />],
   ],
-  ['Session10TriangleR', [<_10c />, <_10p />, <_10i />, <_10pR />, <_10iR />]],
+  ['Session10TriangleR', [<S10c />, <S10p />, <S10i />, <S10pR />, <S10iR />]],
   [
     'Session11HexagonFlat',
-    [<_11c />, <_11p />, <_11i />, <_11pR />, <_11iR />],
+    [<S11c />, <S11p />, <S11i />, <S11pR />, <S11iR />],
   ],
   [
     'Session12TriangleSharpDown',
-    [<_12c />, <_12p />, <_12i />, <_12pR />, <_12iR />],
+    [<S12c />, <S12p />, <S12i />, <S12pR />, <S12iR />],
   ],
-  ['Session13RhombusV', [<_13c />, <_13p />, <_13i />, <_13pR />, <_13iR />]],
+  ['Session13RhombusV', [<S13c />, <S13p />, <S13i />, <S13pR />, <S13iR />]],
   [
     'Session14TriangleCornerL',
-    [<_14c />, <_14p />, <_14i />, <_14pR />, <_14iR />],
+    [<S14c />, <S14p />, <S14i />, <S14pR />, <S14iR />],
   ],
-  ['Session15Octagon', [<_15c />, <_15p />, <_15i />, <_15pR />, <_15iR />]],
-  ['Session16RhombusH', [<_16c />, <_16p />, <_16i />, <_16pR />, <_16iR />]],
+  ['Session15Octagon', [<S15c />, <S15p />, <S15i />, <S15pR />, <S15iR />]],
+  ['Session16RhombusH', [<S16c />, <S16p />, <S16i />, <S16pR />, <S16iR />]],
   [
     'Session17TriandleCornerR',
-    [<_17c />, <_17p />, <_17i />, <_17pR />, <_17iR />],
+    [<S17c />, <S17p />, <S17i />, <S17pR />, <S17iR />],
   ],
-  ['Session18TriangleL', [<_18c />, <_18p />, <_18i />, <_18pR />, <_18iR />]],
-  ['Session19TriangleB', [<_19c />, <_19p />, <_19i />, <_19pR />, <_19iR />]],
+  ['Session18TriangleL', [<S18c />, <S18p />, <S18i />, <S18pR />, <S18iR />]],
+  ['Session19TriangleB', [<S19c />, <S19p />, <S19i />, <S19pR />, <S19iR />]],
   [
     'Session20TriangleDiagonal',
-    [<_20c />, <_20p />, <_20i />, <_20pR />, <_20iR />],
+    [<S20c />, <S20p />, <S20i />, <S20pR />, <S20iR />],
   ],
 ])
 
