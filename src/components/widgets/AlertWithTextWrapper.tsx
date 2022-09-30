c63dceeff935b1623d202f8ad7411d88ba8cba51
Alina Gendel <alina.gendel@sagebase.org>
2022-09-30 16:01:36 -0700

    update AlertWithText to take a child element

--- src/components/widgets/AlertWithTextWrapper.tsx
+++ src/components/widgets/AlertWithTextWrapper.tsx
@@ -2,11 +2,12 @@ import Alert_Icon from '@assets/alert_icon.svg'
 import makeStyles from '@mui/styles/makeStyles'
 import clsx from 'clsx'
 import React from 'react'
-import {AlertWithText} from './StyledComponents'
+import { AlertWithText } from './StyledComponents'
 
 type AlertWithTextWrapperProps = {
-  text: string
+  text?: string
   className?: string
+  children?: React.ReactNode
 }
 
 const useStyles = makeStyles(theme => ({
@@ -18,19 +19,19 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const AlertWithTextWrapper: React.FunctionComponent<AlertWithTextWrapperProps> =
-  ({text, className}) => {
+  ({ text, children, className }) => {
     const classes = useStyles()
     return (
       <AlertWithText
         icon={
           <img
             src={Alert_Icon}
-            style={{height: '20px'}}
+            style={{ height: '20px' }}
             alt={'error-icon'}></img>
         }
         severity="error"
         className={clsx(classes.errorText, className && className)}>
-        {text}
+        {text || children}
       </AlertWithText>
     )
   }
