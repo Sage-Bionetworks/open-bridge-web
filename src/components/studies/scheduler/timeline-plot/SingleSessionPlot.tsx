e601843aa1b5f5ffdd1ba55108ea9572ec0baec8
Alina Gendel <alina.gendel@sagebase.org>
2022-12-13 16:40:32 -0800

    redo the plot without table

--- src/components/studies/scheduler/timeline-plot/SingleSessionPlot.tsx
+++ src/components/studies/scheduler/timeline-plot/SingleSessionPlot.tsx
@@ -1,5 +1,6 @@
 import SessionIcon from '@components/widgets/SessionIcon'
-import {Tooltip} from '@mui/material'
+import {Box, styled, Tooltip} from '@mui/material'
+import {shouldForwardProp} from '@style/theme'
 import React from 'react'
 import {CoordItem} from './utility'
 export interface SingleSessionPlotProps {
@@ -12,6 +13,22 @@ export interface SingleSessionPlotProps {
   weekSessionNumber: number
   xCoords: CoordItem[]
 }
+
+const StyledDayContainer = styled(Box, {label: 'StyledDayContainer', shouldForwardProp: shouldForwardProp})<{
+  $unitPixelWidth: number
+  $hasTopBorder: boolean
+}>(({theme, $unitPixelWidth, $hasTopBorder}) => ({
+  width: $unitPixelWidth + 'px',
+  //borderBottom: '1px solid black',
+  borderLeft: '1px solid #EAECEE',
+  display: 'flex',
+  justifyContent: 'space-around',
+  borderTop: $hasTopBorder ? '1px solid #EAECEE' : 'none',
+  // borderRight: index === 6 ? '1px solid black' : 'none',
+  height: '38px',
+  padding: 0,
+  position: 'relative',
+}))
 
 const SessionPlot: React.FunctionComponent<SingleSessionPlotProps> = ({
   sessionIndex,
@@ -37,48 +54,35 @@ const SessionPlot: React.FunctionComponent<SingleSessionPlotProps> = ({
       }}></div>
   ))
 
-  const table = (
-    <table style={{borderCollapse: 'collapse', width: '100%'}}>
-      <tr>
-        {[...new Array(7)].map((i, index) => (
-          <td
-            style={{
-              width: unitPixelWidth + 'px',
-              //borderBottom: '1px solid black',
-              borderLeft: '1px solid #EAECEE',
+  const table = [...new Array(7)].map((_i, index) => {
+    const coords = xCoords.filter(c => Math.floor(c.c) === index)
 
-              borderTop: weekSessionNumber > 0 ? '1px solid #EAECEE' : 'none',
-              // borderRight: index === 6 ? '1px solid black' : 'none',
-              height: '38px',
-              padding: 0,
-            }}></td>
+    return (
+      <StyledDayContainer $unitPixelWidth={unitPixelWidth} $hasTopBorder={weekSessionNumber > 0} key={_i}>
+        {coords.map(({c: i, expiration}) => (
+          <Tooltip
+            title={expiration}
+            placement="top"
+            key={`session${i}_${unitPixelWidth * i}_${sessionIndex}_${lineNumber}`}>
+            <div
+              id={`session_${i}`}
+              style={{
+                position: 'absolute',
+                zIndex: 100,
+
+                top: `${topOffset}px`,
+                left: `${(i - index) * unitPixelWidth - 6}px`,
+              }}>
+              {/*i*/}
+              <SessionIcon symbolKey={sessionSymbol} index={sessionIndex}></SessionIcon>
+            </div>
+          </Tooltip>
         ))}
-      </tr>
-    </table>
-  )
-  const sessionGraph = xCoords.map(({c: i, expiration}) => (
-    <Tooltip title={expiration} placement="top" key={`session${i}_${unitPixelWidth * i}_${sessionIndex}_${lineNumber}`}>
-      <div
-        id={`session_${i}`}
-        style={{
-          position: 'absolute',
-          zIndex: 100,
+      </StyledDayContainer>
+    )
+  })
 
-          top: `${topOffset}px`,
-          left: `${i * unitPixelWidth - 6}px`,
-        }}>
-        {/*i*/}
-        <SessionIcon symbolKey={sessionSymbol} index={sessionIndex}></SessionIcon>
-      </div>
-    </Tooltip>
-  ))
-  return (
-    <div style={{position: 'relative', height: '100%', width: '100%'}}>
-      {table}
-      {/*dayDividers*/}
-      {sessionGraph}
-    </div>
-  )
+  return <div style={{position: 'relative', height: '100%', width: '100%', display: 'flex'}}>{table}</div>
 }
 
 export default React.memo(SessionPlot)
