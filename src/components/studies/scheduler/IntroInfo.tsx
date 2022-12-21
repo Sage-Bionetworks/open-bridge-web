5f4b39244689507df8ff983cddd64982ab7a09db
Alina Gendel <alina.gendel@sagebase.org>
2022-12-20 16:46:11 -0800

    turn initial study details into a nav item page

--- src/components/studies/scheduler/IntroInfo.tsx
+++ src/components/studies/scheduler/IntroInfo.tsx
@@ -5,19 +5,19 @@ import {Alert, Box, Button, Container, Divider, FormControlLabel, Theme} from '@
 import createStyles from '@mui/styles/createStyles'
 import makeStyles from '@mui/styles/makeStyles'
 import ScheduleService from '@services/schedule.service'
-import {useUpdateSchedule} from '@services/scheduleHooks'
+import {useSchedule, useUpdateSchedule} from '@services/scheduleHooks'
 import {useStudy, useUpdateStudyDetail} from '@services/studyHooks'
-import {latoFont, poppinsFont} from '@style/theme'
+import {latoFont, theme} from '@style/theme'
 import constants from '@typedefs/constants'
 import {DWsEnum, Schedule} from '@typedefs/scheduling'
 import {Study} from '@typedefs/types'
 import React from 'react'
+import {BuilderWrapper} from '../StudyBuilder'
 import Duration from './Duration'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     labelDuration: {
-      fontFamily: poppinsFont,
       marginLeft: 0,
       fontSize: '18px',
       fontWeight: 600,
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
     container: {
       display: 'flex',
       flexDirection: 'row',
-      padding: theme.spacing(3.75),
+
       minWidth: '300px',
     },
     formControl: {
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     headerText: {
       fontSize: '18px',
-      fontFamily: 'Poppins',
+
       lineHeight: '27px',
     },
     description: {
@@ -90,128 +90,189 @@ const useStyles = makeStyles((theme: Theme) =>
 export interface IntroInfoProps {
   studyName: string
   id: string
+
+  children: React.ReactNode
+  onShowFeedback: Function
 }
 
-const IntroInfo: React.FunctionComponent<IntroInfoProps> = ({studyName: name, id: studyId}: IntroInfoProps) => {
+const IntroInfo: React.FunctionComponent<IntroInfoProps> = ({id, onShowFeedback}) => {
   const classes = useStyles()
   const {token} = useUserSessionDataState()
-  const [studyName, setStudyName] = React.useState<any>(name === constants.constants.NEW_STUDY_NAME ? '' : name)
+  const [studyName, setStudyName] = React.useState('')
   const [duration, setDuration] = React.useState<any>('')
-  const {data: study, error: studyError} = useStudy(studyId)
-  const {mutateAsync: mutateSchedule} = useUpdateSchedule()
+  const {data: study, error: studyError} = useStudy(id)
+  const {data: schedule, error: scheduleError} = useSchedule(id)
+  const {mutate: mutateSchedule} = useUpdateSchedule()
 
-  const {mutateAsync: mutateStudy} = useUpdateStudyDetail()
+  const {mutate: mutateStudy} = useUpdateStudyDetail()
 
-  const createScheduleAndNameStudy = async (studyId: string, studyName: string, duration: string, start: string) => {
-    const studySession = ScheduleService.createEmptyScheduleSession(start, SessionSymbols.keys().next().value)
-    let schedule: Schedule = {
-      guid: '',
-      name: studyId,
-      duration,
-      sessions: [studySession],
+  React.useEffect(() => {
+    console.log('getting study')
+    if (study && study.name !== constants.constants.NEW_STUDY_NAME) {
+      setStudyName(study.name)
     }
-    const newSchedule = await ScheduleService.createSchedule(studyId, schedule, token!)
+  }, [study])
 
-    //agendel 10/6 adding  default to signin with study Id signInTypes: SignInType[]
-    const updatedStudy: Study = {
-      ...study!,
-      name: studyName,
-      signInTypes: ['external_id_password'],
+  React.useEffect(() => {
+    console.log('getting schedule')
+    if (schedule && !scheduleError) {
+      console.log('setting duration', schedule.duration)
+      setDuration(schedule.duration)
+    }
+  }, [schedule, scheduleError])
+
+  const createScheduleAndNameStudy = async (id: string, studyName: string, duration: string, start: string) => {
+    const shouldUpdateSchedule = !schedule || schedule.duration !== duration
+    const shouldUpdateStudy = study && study.name !== studyName
+    var hasError = false
+
+    //update schedule if needed
+    if (shouldUpdateSchedule) {
+      let updatedSchedule: Schedule
+      let action: 'UPDATE' | 'CREATE' = 'CREATE'
+      if (!schedule) {
+        //creating new schedule
+        const studySession = ScheduleService.createEmptyScheduleSession(start, SessionSymbols.keys().next().value)
+        updatedSchedule = {
+          guid: '',
+          name: id,
+          duration,
+          sessions: [studySession],
+        }
+      } else {
+        //updating existing schedule
+        action = 'UPDATE'
+        updatedSchedule = {
+          ...schedule,
+          duration,
+        }
+      }
+
+      mutateSchedule(
+        {
+          studyId: id,
+          schedule: updatedSchedule,
+          action,
+        },
+        {
+          onError: (e: any) => {
+            onShowFeedback(e)
+            hasError = true
+          },
+          onSuccess: () => console.log(`schedule ${action}d`),
+        }
+      )
     }
 
-    mutateSchedule({
-      studyId: studyId,
-      schedule: newSchedule,
-      action: 'CREATE',
-    }).then(s => console.log('schedule created'))
+    if (shouldUpdateStudy) {
+      const updatedStudy: Study = {
+        ...study!,
+        name: studyName,
+      }
 
-    mutateStudy({study: updatedStudy}).then(e => {
-      console.log('study updated')
-    })
+      mutateStudy(
+        {study: updatedStudy},
+        {
+          onError: (e: any) => {
+            onShowFeedback(e)
+            hasError = true
+          },
+          onSuccess: () => console.log(`study updated`),
+        }
+      )
+    }
+    if (!hasError) {
+      history.pushState(null, '', `/studies/builder/${id}/session-creator`)
+    }
   }
 
   return (
-    <Container maxWidth="md" className={classes.container}>
-      <div>
-        {studyError && <Alert color="error">{studyError.message}</Alert>}
-        <FormControlLabel
-          style={{marginBottom: '35px', marginLeft: 0}}
-          classes={{labelPlacementStart: classes.labelDuration}}
-          label={
-            <Box width="210px" marginRight="40px">
-              <strong className={classes.headerText}>Study Name</strong>
-              <br /> <br />
-              <div className={classes.description}>
-                This name will be displayed to your participants in the app.
-              </div>{' '}
+    <>
+      <BuilderWrapper sectionName="Study Details">
+        <Container maxWidth="md" className={classes.container}>
+          <div>
+            {studyError && <Alert color="error">{studyError.message}</Alert>}
+            <FormControlLabel
+              style={{marginBottom: '35px', marginLeft: 0}}
+              classes={{labelPlacementStart: classes.labelDuration}}
+              label={
+                <Box width="210px" marginRight="40px">
+                  <strong className={classes.headerText}>Study Name</strong>
+                  <br /> <br />
+                  <div className={classes.description}>
+                    This name will be displayed to your participants in the app.
+                  </div>{' '}
+                </Box>
+              }
+              className={classes.formControl}
+              labelPlacement="start"
+              control={<SimpleTextInput fullWidth value={studyName} onChange={e => setStudyName(e.target.value)} />}
+            />
+            <Divider className={classes.divider}></Divider>
+            <Box className={classes.middleContainer}>
+              <FormControlLabel
+                classes={{
+                  labelPlacementStart: classes.labelDuration,
+                }}
+                label={
+                  <Box width="210px" marginRight="40px">
+                    <strong className={classes.headerText}>How long is your study?</strong>
+                    <br /> <br />
+                    <div className={classes.description}>
+                      This is the duration that a participant is involved in the study.
+                    </div>{' '}
+                  </Box>
+                }
+                className={classes.formControl}
+                labelPlacement="start"
+                control={
+                  <Box>
+                    <Duration
+                      onChange={e => setDuration(e.target.value)}
+                      durationString={duration || ''}
+                      unitLabel="study duration unit"
+                      numberLabel="study duration number"
+                      maxDurationDays={1825}
+                      unitData={DWsEnum}
+                      isIntro={true}></Duration>
+                    <span className={classes.hint}>
+                      <strong>The study duration must be shorter than 5 years.</strong>
+                    </span>
+                  </Box>
+                }
+              />
             </Box>
-          }
-          className={classes.formControl}
-          labelPlacement="start"
-          control={<SimpleTextInput fullWidth value={studyName} onChange={e => setStudyName(e.target.value)} />}
-        />
-        <Divider className={classes.divider}></Divider>
-        <Box className={classes.middleContainer}>
-          <FormControlLabel
-            classes={{
-              labelPlacementStart: classes.labelDuration,
-            }}
-            label={
-              <Box width="210px" marginRight="40px">
-                <strong className={classes.headerText}>How long is your study?</strong>
-                <br /> <br />
-                <div className={classes.description}>
-                  This is the duration that a participant is involved in the study.
-                </div>{' '}
-              </Box>
-            }
-            className={classes.formControl}
-            labelPlacement="start"
-            control={
-              <Box>
-                <Duration
-                  onChange={e => setDuration(e.target.value)}
-                  durationString={duration || ''}
-                  unitLabel="study duration unit"
-                  numberLabel="study duration number"
-                  maxDurationDays={1825}
-                  unitData={DWsEnum}
-                  isIntro={true}></Duration>
-                <span className={classes.hint}>
-                  <strong>The study duration must be shorter than 5 years.</strong>
-                </span>
-              </Box>
-            }
-          />
-        </Box>
-        <Button
-          className={classes.continueButton}
-          variant="contained"
-          color="primary"
-          key="saveButton"
-          onClick={e =>
-            createScheduleAndNameStudy(
-              studyId,
+          </div>
 
-              studyName,
-              duration,
-              'timeline_retrieved'
-            )
-          }
-          disabled={!(duration && studyName)}>
-          Continue
-        </Button>
-      </div>
+          <ul className={classes.weekInformation}>
+            <li>Example Conversions</li>
+            <li>1 year = 52 weeks</li>
+            <li>2 year = 104 weeks</li>
+            <li>3 year = 156 weeks</li>
+            <li>4 year = 208 weeks</li>
+            <li>5 year = 260 weeks</li>
+          </ul>
+        </Container>
+      </BuilderWrapper>
+      <Button
+        // className={classes.continueButton}
+        variant="contained"
+        sx={{marginTop: theme.spacing(2)}}
+        color="primary"
+        key="saveButton"
+        onClick={e =>
+          createScheduleAndNameStudy(
+            id,
 
-      <ul className={classes.weekInformation}>
-        <li>Example Conversions</li>
-        <li>1 year = 52 weeks</li>
-        <li>2 year = 104 weeks</li>
-        <li>3 year = 156 weeks</li>
-        <li>4 year = 208 weeks</li>
-        <li>5 year = 260 weeks</li>
-      </ul>
-    </Container>
+            studyName,
+            duration,
+            'timeline_retrieved'
+          )
+        }
+        disabled={!(duration && studyName)}>
+        Continue
+      </Button>
+    </>
   )
 }
 
