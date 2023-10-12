4cd44fa42683a5a2126cf5a1bde9708726c8a477
Shannon Young <shannon.young@sagebase.org>
2023-10-12 16:26:27 -0700

    Update src/components/widgets/ReadOnlyBanner.tsx
    Co-authored-by: hallieswan <26949006+hallieswan@users.noreply.github.com>

--- src/components/widgets/ReadOnlyBanner.tsx
+++ src/components/widgets/ReadOnlyBanner.tsx
@@ -1,9 +1,9 @@
 import { Box } from '@mui/material'
 import { theme } from '@style/theme'
 import ErrorTwoToneIcon from '@mui/icons-material/ErrorTwoTone'
-import React, {FunctionComponent} from 'react'
+import React from 'react'
 
-const ReadOnlyBanner: React.FunctionComponent<{label: String}> = ({
+const ReadOnlyBanner: React.FunctionComponent<{label: string}> = ({
   label,
 }) => <Box
   sx={{
