ddbca9cc4737a9d04da8f4ff8293f8382d957353
Shannon Young <shannon.young@sagebase.org>
2023-10-11 23:19:47 -0700

    DHP-1087 Update copy on the completion screen and remove “Publish” button

--- src/components/surveys/survey-design/question-edit/rhs-subcontrols/Completion.tsx
+++ src/components/surveys/survey-design/question-edit/rhs-subcontrols/Completion.tsx
@@ -27,9 +27,10 @@ const Completion: React.FunctionComponent<{
       <StyledText>
         Customize the Completion Screen that participants will see after they've submitted their responses. <br />
         <br />
-        Surveys will be submitted on the screen prior to this Completion screen.
+        Surveys will be submitted on the screen prior to this Completion Screen.
       </StyledText>{' '}
       <Divider sx={{color: '#DFE2E6', margin: theme.spacing(4, -6, 4, -6)}} />
+      {/* TODO: syoung 10/11/2023 Figure out the copy for survey access settings and that this means to design.
       <Typography sx={{fontSize: '20px'}}>Invite Others to Your Survey </Typography>
       <StyledText>Some content about inviting others to your survey to review it. </StyledText>
       <Button
@@ -38,9 +39,9 @@ const Completion: React.FunctionComponent<{
         sx={{padding: theme.spacing(1, 2.5), margin: theme.spacing(1.5, 0, 5, 0)}}>
         {' '}
         Survey Access Settings
-      </Button>
-      <Typography sx={{fontSize: '20px'}}> Ready to launch?</Typography>
-      <StyledText> Before launching, please review any branching logic to your questions. </StyledText>
+      </Button> */}
+      <Typography sx={{fontSize: '20px', margin: theme.spacing(0, 0, 1, 0)}}> Ready to launch?</Typography>
+      <StyledText> Before launching, please review any branching logic used by your questions. </StyledText>
       <Button
         color="primary"
         variant="contained"
@@ -49,21 +50,18 @@ const Completion: React.FunctionComponent<{
         sx={{padding: theme.spacing(1, 2.5), margin: theme.spacing(1.5, 0, 5, 0)}}>
         Preview Branching Logic
       </Button>
-      <Typography sx={{fontSize: '20px'}}>Publish Survey</Typography>
+      <Typography sx={{fontSize: '20px', margin: theme.spacing(0, 0, 1, 0)}}>Publish Survey</Typography>
       <Typography
-        sx={{fontStyle: 'italic', fontWeight: 400, fontSize: '14px', display: 'flex', marginTop: theme.spacing(2)}}>
-        <Alert sx={{color: '#ff4163'}} />
-        Please note that once a survey is published, it cannot be deleted if it is used in any study on the platform.
+        sx={{fontStyle: 'italic', fontWeight: 400, fontSize: '14px', display: 'flex', marginTop: theme.spacing(2), marginBottom: theme.spacing(1)}}>
+        <Alert sx={{color: '#ff4163', margin: theme.spacing(0, 1, 0, 0)}} />
+        Please note that once a survey is published, it cannot be edited or deleted.
       </Typography>
-      <Button
-        color="primary"
-        variant="contained"
-        startIcon={<FileUploadTwoToneIcon />}
-        onClick={() => alert('TODO')}
-        sx={{padding: theme.spacing(1, 2.5), margin: theme.spacing(1.5, 0, 5, 0)}}>
-        Publish Survey
-      </Button>
-      <Button> </Button>
+      <StyledText> 
+        A survey is published automatically the first time a study that includes the survey launches.
+        <br />
+        <br />
+        Before launching your study, preview the study - including all surveys - on a mobile device.
+      </StyledText>
     </Box>
   )
 }
